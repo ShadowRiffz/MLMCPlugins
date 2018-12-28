f333a63294e462a8f0a6d2bb8032c40cc6d954ef
Alex Z. <alexz1012@yahoo.com>
2018-12-28 01:08:32 -0800

    Finish mason charm command

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Commands/MasonCommands.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Commands/MasonCommands.java
@@ -32,7 +32,31 @@ public class MasonCommands implements CommandExecutor {
 			
 			}
 			else if(args[0].equalsIgnoreCase("create")) {
-				if(args[1].equalsIgnoreCase("slot")) {
+				if (args[1].equalsIgnoreCase("basic")) {
+					if (args[2].equalsIgnoreCase("exp") ||
+							args[2].equalsIgnoreCase("drop") ||
+							args[2].equalsIgnoreCase("looting") ||
+							args[2].equalsIgnoreCase("traveler") ||
+							args[2].equalsIgnoreCase("recovery")) {
+						masonMethods.createBasicCharm(p, args[2].toLowerCase());
+					}
+					else {
+						Util.sendMessage(p, "&cInvalid charm!");
+					}
+				}
+				else if (args[1].equalsIgnoreCase("advanced")) {
+					if (args[2].equalsIgnoreCase("exp") ||
+							args[2].equalsIgnoreCase("drop") ||
+							args[2].equalsIgnoreCase("looting") ||
+							args[2].equalsIgnoreCase("secondchance") ||
+							args[2].equalsIgnoreCase("hunger")) {
+						masonMethods.createAdvancedCharm(p, args[2].toLowerCase());
+					}
+					else {
+						Util.sendMessage(p, "&cInvalid charm!");
+					}
+				}
+				else if(args[1].equalsIgnoreCase("slot")) {
 					if(StringUtils.isNumeric(args[2])) {
 						masonMethods.createSlot(p, Integer.parseInt(args[2]));
 						return true;
@@ -41,6 +65,10 @@ public class MasonCommands implements CommandExecutor {
 						Util.sendMessage(p, "&cInvalid level!");
 						return true;
 					}
+				}
+				else {
+					Util.sendMessage(p, "&cInvalid item!");
+					return true;
 				}
 			}
 			else if(args[0].equalsIgnoreCase("slot")) {
--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Methods/MasonMethods.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Methods/MasonMethods.java
@@ -20,6 +20,40 @@ public class MasonMethods {
 	static final int ENGRAVE_GOLD_BASE = -10000;
 	static final int ENGRAVE_GOLD_PER_LVL = 15000;
 	
+	// Prices
+	static final int BASIC_EXP_GOLD = 5000;
+	static final int BASIC_EXP_ESSENCE = 10;
+	static final int BASIC_EXP_LEVEL = 2;
+	static final int BASIC_DROP_GOLD = 10000;
+	static final int BASIC_DROP_ESSENCE = 16;
+	static final int BASIC_DROP_LEVEL = 3;
+	static final int BASIC_LOOTING_GOLD = 25000;
+	static final int BASIC_LOOTING_ESSENCE = 24;
+	static final int BASIC_LOOTING_LEVEL = 2;
+	static final int BASIC_TRAVELER_GOLD = 3000;
+	static final int BASIC_TRAVELER_ESSENCE = 8;
+	static final int BASIC_TRAVELER_LEVEL = 1;
+	static final int BASIC_RECOVERY_GOLD = 10000;
+	static final int BASIC_RECOVERY_ESSENCE = 16;
+	static final int BASIC_RECOVERY_LEVEL = 2;
+
+	static final int ADVANCED_HUNGER_GOLD = 25000;
+	static final int ADVANCED_HUNGER_ESSENCE = 32;
+	static final int ADVANCED_HUNGER_LEVEL = 4;
+	static final int ADVANCED_EXP_GOLD = 5000;
+	static final int ADVANCED_EXP_ESSENCE = 16;
+	static final int ADVANCED_EXP_LEVEL = 3;
+	static final int ADVANCED_DROP_GOLD = 10000;
+	static final int ADVANCED_DROP_ESSENCE = 24;
+	static final int ADVANCED_DROP_LEVEL = 4;
+	static final int ADVANCED_LOOTING_GOLD = 50000;
+	static final int ADVANCED_LOOTING_ESSENCE = 36;
+	static final int ADVANCED_LOOTING_LEVEL = 3;
+	static final int ADVANCED_SECONDCHANCE_GOLD = 25000;
+	static final int ADVANCED_SECONDCHANCE_ESSENCE = 24;
+	static final int ADVANCED_SECONDCHANCE_LEVEL = 4;
+	
+	
 	public MasonMethods(Main main) {
 		this.main = main;
 		this.econ = main.getEconomy();
@@ -52,6 +86,118 @@ public class MasonMethods {
 			}
 			else {
 				Util.sendMessage(p, "&cThis item cannot have any more slots!");
+			}
+		}
+		else {
+			Util.sendMessage(p, "&cYou do not yet have the required skill for this tier!");
+		}
+	}
+
+	public void createBasicCharm(Player p, String type) {
+		if (p.hasPermission("mason.charm.basic")) {
+			if (p.getInventory().firstEmpty() != -1) {
+				int gold = 0;
+				int essence = 0;
+				int level = 0;
+				switch (type) {
+				case "exp":
+					gold = BASIC_EXP_GOLD;
+					essence = BASIC_EXP_ESSENCE;
+					level = BASIC_EXP_LEVEL;
+					break;
+				case "drop":
+					gold = BASIC_DROP_GOLD;
+					essence = BASIC_DROP_ESSENCE;
+					level = BASIC_DROP_LEVEL;
+					break;
+				case "looting":
+					gold = BASIC_LOOTING_GOLD;
+					essence = BASIC_LOOTING_ESSENCE;
+					level = BASIC_LOOTING_LEVEL;
+					break;
+				case "traveler":
+					gold = BASIC_TRAVELER_GOLD;
+					essence = BASIC_TRAVELER_ESSENCE;
+					level = BASIC_TRAVELER_LEVEL;
+					break;
+				case "recovery":
+					gold = BASIC_RECOVERY_GOLD;
+					essence = BASIC_RECOVERY_ESSENCE;
+					level = BASIC_RECOVERY_LEVEL;
+					break;
+				}
+				if(p.getInventory().containsAtLeast(CommonItems.getEssence(level), essence)) {
+					if(econ.has(p, gold)) {
+						p.getInventory().removeItem(Util.setAmount(CommonItems.getEssence(level), essence));
+						econ.withdrawPlayer(p, gold);
+						Util.sendMessage(p, "&7Successfully created charm!");
+					}
+					else {
+						Util.sendMessage(p, "&cYou lack the gold to create this!");
+					}
+				}
+				else {
+					Util.sendMessage(p, "&cYou lack the materials to create this!");
+				}
+			}
+			else {
+				Util.sendMessage(p, "&cYour inventory is full!");
+			}
+		}
+		else {
+			Util.sendMessage(p, "&cYou do not yet have the required skill for this tier!");
+		}
+	}
+
+	public void createAdvancedCharm(Player p, String type) {
+		if (p.hasPermission("mason.charm.advanced")) {
+			if (p.getInventory().firstEmpty() != -1) {
+				int gold = 0;
+				int essence = 0;
+				int level = 0;
+				switch (type) {
+				case "exp":
+					gold = ADVANCED_EXP_GOLD;
+					essence = ADVANCED_EXP_ESSENCE;
+					level = ADVANCED_EXP_LEVEL;
+					break;
+				case "drop":
+					gold = ADVANCED_DROP_GOLD;
+					essence = ADVANCED_DROP_ESSENCE;
+					level = ADVANCED_DROP_LEVEL;
+					break;
+				case "looting":
+					gold = ADVANCED_LOOTING_GOLD;
+					essence = ADVANCED_LOOTING_ESSENCE;
+					level = ADVANCED_LOOTING_LEVEL;
+					break;
+				case "hunger":
+					gold = ADVANCED_HUNGER_GOLD;
+					essence = ADVANCED_HUNGER_ESSENCE;
+					level = ADVANCED_HUNGER_LEVEL;
+					break;
+				case "secondchance":
+					gold = ADVANCED_SECONDCHANCE_GOLD;
+					essence = ADVANCED_SECONDCHANCE_ESSENCE;
+					level = ADVANCED_SECONDCHANCE_LEVEL;
+					break;
+				}
+				if(p.getInventory().containsAtLeast(CommonItems.getEssence(level), essence)) {
+					if(econ.has(p, gold)) {
+						p.getInventory().removeItem(Util.setAmount(CommonItems.getEssence(level), essence));
+						econ.withdrawPlayer(p, gold);
+						Util.sendMessage(p, "&7Successfully created charm!");
+					}
+					else {
+						Util.sendMessage(p, "&cYou lack the gold to create this!");
+					}
+				}
+				else {
+					Util.sendMessage(p, "&cYou lack the materials to create this!");
+				}
+			}
+			else {
+				Util.sendMessage(p, "&cYour inventory is full!");
 			}
 		}
 		else {
