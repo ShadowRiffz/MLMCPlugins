337f0822c9135afe3ab54dc774387c737df29fac
alexz <alexz@100.81.38.215>
2019-01-10 16:01:14 -0800

    Add quick drink

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Listeners/CulinarianListeners.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Listeners/CulinarianListeners.java
@@ -4,15 +4,18 @@ import java.util.HashMap;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.CraftingInventory;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 
 import com.sucy.skill.SkillAPI;
@@ -43,6 +46,261 @@ public class CulinarianListeners implements Listener {
 			}
 		}, 0, 100L);
 		gen = new Random();
+	}
+	
+	@EventHandler
+	public void onRightClick(PlayerInteractEvent e) {
+		if(e.getPlayer() == null || !(e.getPlayer() instanceof Player) || e.getHand().equals(EquipmentSlot.OFF_HAND)) {
+			return;
+		}
+		Player p = e.getPlayer();
+		ItemStack item = p.getInventory().getItemInMainHand();
+
+		boolean quickEat = false;
+		for(String line : item.getItemMeta().getLore()) {
+			if(line.contains("Quick Eat")) {
+				quickEat = true;
+				break;
+			}
+		}
+		if(quickEat) {
+			ItemStack[] contents = p.getInventory().getContents();
+			boolean canGetAttrs = p.getWorld().getName().equalsIgnoreCase("Argyll") ||
+					p.getWorld().getName().equalsIgnoreCase("ClassPVP");
+			for(int i = 9; i < 36; i++) {
+				if(contents[i] != null && contents[i].hasItemMeta() && contents[i].getItemMeta().hasLore()) {
+					String id = contents[i].getItemMeta().getLore().get(0);
+
+					if(id.contains("Drink 1")) {
+						int drunk = 27 + gen.nextInt(7);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.02, "Spirit", 40);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 2")) {
+						int drunk = 22 + gen.nextInt(6);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.02, "Spirit", 40);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 3")) {
+						int drunk = 17 + gen.nextInt(6);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.005, "Endurance", 60);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 4")) {
+						int drunk = 17 + gen.nextInt(6);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.01, "Vitality", 50);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 5")) {
+						int drunk = 27 + gen.nextInt(7);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.03, "Spirit", 40);
+							addStat(p, -0.03, "Strength", 40);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 6")) {
+						int drunk = 17 + gen.nextInt(6);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.05, "Strength", 30);
+							addStat(p, -0.02, "Endurance", 30);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 7")) {
+						int drunk = 30 + gen.nextInt(31);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.02, "Intelligence", 40);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 8")) {
+						int drunk = 12 + gen.nextInt(7);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.02, "Perception", 35);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 9")) {
+						int drunk = 35 + gen.nextInt(10);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.03, "Dexterity", 30);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 10")) {
+						int drunk = 35 + gen.nextInt(10);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.02, "Strength", 40);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 11")) {
+						int drunk = 27 + gen.nextInt(7);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.02, "Intelligence", 40);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 12")) {
+						int drunk = 12 + gen.nextInt(7);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.02, "Vitality", 40);
+							addStat(p, -0.02, "Spirit", 40);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 13")) {
+						int drunk = 27 + gen.nextInt(7);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.02, "Spirit", 40);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 14")) {
+						int drunk = 40 + gen.nextInt(9);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.04, "Endurance", 20);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 15")) {
+						int drunk = 35 + gen.nextInt(10);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.04, "Intelligence", 20);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+					else if(id.contains("Drink 16")) {
+						int drunk = 27 + gen.nextInt(7);
+						if(drunkness.containsKey(p)) {
+							drunkness.put(p, drunkness.get(p) + drunk);
+						} else {
+							drunkness.put(p, drunk);
+						}
+						CulinarianUtils.checkAlcoholUp(p, drunkness.get(p), drunkness);
+						if(canGetAttrs) {
+							addStat(p, 0.03, "Strength", 30);
+						}
+				        p.getWorld().playSound(p.getEyeLocation(), Sound.ENTITY_GENERIC_DRINK, 1.0F, 1.0F);
+						break;
+					}
+				}
+			}
+		}
 	}
 	
 	@EventHandler
