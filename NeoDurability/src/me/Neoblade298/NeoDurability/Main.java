37859c9b69ae4d3331859110c5a0e64603577eee
Alex Z <alexz1012@yahoo.com>
2019-02-02 15:55:14 -0800

    Fix null pointer with no lore

--- NeoDurability/src/me/Neoblade298/NeoDurability/Main.java
+++ NeoDurability/src/me/Neoblade298/NeoDurability/Main.java
@@ -95,7 +95,7 @@ public class Main
   	      if (!off.equals(new ItemStack(Material.AIR)) && off.hasItemMeta() && off.getItemMeta().hasLore() && off.getItemMeta().getLore().get(0).contains("Tier")) {
   	        FlagManager.addFlag(player, "WeaponDur", 20);
   	        reduceDurability(off, player, 5);
-  	      } else if (!main.equals(new ItemStack(Material.AIR)) && main.hasItemMeta() && main.getItemMeta().getLore().get(0).contains("Tier")) {
+  	      } else if (!main.equals(new ItemStack(Material.AIR)) && main.hasItemMeta() && main.getItemMeta().hasLore() && main.getItemMeta().getLore().get(0).contains("Tier")) {
   	        FlagManager.addFlag(player, "WeaponDur", 20);
   	        reduceDurability(main, player, 0);
   	      }
@@ -160,7 +160,7 @@ public class Main
   	      if (!off.equals(new ItemStack(Material.AIR)) && off.hasItemMeta() && off.getItemMeta().hasLore() && off.getItemMeta().getLore().get(0).contains("Tier")) {
   	        FlagManager.addFlag(player, "WeaponDur", 20);
   	        reduceDurability(off, player, 5);
-  	      } else if (!main.equals(new ItemStack(Material.AIR)) && main.hasItemMeta() && main.getItemMeta().getLore().get(0).contains("Tier")) {
+  	      } else if (!main.equals(new ItemStack(Material.AIR)) && main.hasItemMeta() && main.getItemMeta().hasLore() && main.getItemMeta().getLore().get(0).contains("Tier")) {
   	        FlagManager.addFlag(player, "WeaponDur", 20);
   	        reduceDurability(main, player, 0);
   	      }
@@ -192,7 +192,7 @@ public class Main
 	      if (!off.equals(new ItemStack(Material.AIR)) && off.hasItemMeta() && off.getItemMeta().hasLore() && off.getItemMeta().getLore().get(0).contains("Tier")) {
 	        FlagManager.addFlag(player, "WeaponDur", 20);
 	        reduceDurability(off, player, 5);
-	      } else if (!main.equals(new ItemStack(Material.AIR)) && main.hasItemMeta() && main.getItemMeta().getLore().get(0).contains("Tier")) {
+	      } else if (!main.equals(new ItemStack(Material.AIR)) && main.hasItemMeta() && main.getItemMeta().hasLore() && main.getItemMeta().getLore().get(0).contains("Tier")) {
 	        FlagManager.addFlag(player, "WeaponDur", 20);
 	        reduceDurability(main, player, 0);
 	      }
