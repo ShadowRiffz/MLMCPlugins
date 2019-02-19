af3693fad7e41b3171aac8759882bfa579932741
Alex Z. <alexz1012@yahoo.com>
2019-02-18 16:28:46 -0800

    Complete plugin

--- NeoAttrSetter/src/me/neoblade298/neoattrsetter/Commands.java
+++ NeoAttrSetter/src/me/neoblade298/neoattrsetter/Commands.java
@@ -1,32 +1,130 @@
 package me.neoblade298.neoattrsetter;
 
-import org.bukkit.Bukkit;
+import java.util.ArrayList;
+
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.sucy.skill.SkillAPI;
+import com.sucy.skill.api.player.PlayerData;
 
 
 public class Commands implements CommandExecutor{
 	
 	Main main;
+	ArrayList<String> validAttrs;
 	
 	public Commands(Main main) {
 		this.main = main;
+		validAttrs = new ArrayList<String>();
+		validAttrs.add("strength");
+		validAttrs.add("dexterity");
+		validAttrs.add("intelligence");
+		validAttrs.add("spirit");
+		validAttrs.add("endurance");
+		validAttrs.add("perception");
+		validAttrs.add("str");
+		validAttrs.add("dex");
+		validAttrs.add("int");
+		validAttrs.add("spr");
+		validAttrs.add("prc");
+		validAttrs.add("end");
 	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
 		
-		if(args.length == 1) {
-			if(Bukkit.getPlayer(args[0]) != null) {
-				return true;
+		if(args.length > 1) {
+			if(sender instanceof Player) {
+				Player p = (Player) sender;
+				if(p.getWorld().getName().equals("Argyll") ||
+						p.getWorld().getName().equals("ClassPVP")) {
+					PlayerData data = SkillAPI.getPlayerData(p);
+					
+					// attrs reset
+					if(args.length == 1 && args[0].equalsIgnoreCase("reset")) {
+						data.refundAttributes();
+						return true;
+					}
+					// attrs set [attr] [amt]
+					else if(args.length == 3 && args[0].equalsIgnoreCase("set")) {
+						if(validAttrs.contains(args[1].toLowerCase())) {
+							if(StringUtils.isNumeric(args[2])) {
+								int newNum = Integer.parseInt(args[2]);
+								if(newNum < 0) {
+									int oldNum = data.getInvestedAttribute(args[1]);
+									int diff = newNum - oldNum;
+									if(diff > 0) {
+										if(data.getAttributePoints() >= diff) {
+											for(int i = diff; i > 0; i--) {
+												data.upAttribute(args[1]);
+											}
+											msgP(p, "&7Success! Attribute set.");
+											return true;
+										}
+										else {
+											msgP(p, "&cNot enough attribute points to invest!");
+											return true;
+										}
+									}
+									else if(diff < 0) {
+										for(int i = diff; i > 0; i--) {
+											data.refundAttribute(args[1]);
+										}
+										msgP(p, "&7Success! Attribute set.");
+										return true;
+									}
+								}
+								else {
+									msgP(p, "&cThe number you specify must be positive!");
+									return true;
+								}
+							}
+							else {
+								msgP(p, "&cYou must specify a number to set the attribute to!");
+								return true;
+							}
+						}
+						else {
+							msgP(p, "&cInvalid attribute!");
+							return true;
+						}
+					}
+					else {
+						msgP(p, "&cInvalid command!");
+						return true;
+					}
+					
+				}
+				else {
+					msgP(p, "&cYou may only adjust attributes in the quest world");
+					return true;
+				}
+			}
+			else {
+				return false;
 			}
 		}
 		
 		if(args.length == 0) {
+			msg(sender, "&7[&c&lAttribute Setter&7]");
+			msg(sender, "&7- &c/attrs &7- Help menu");
+			msg(sender, "&7- &c/attrs set [attr] [amt] &7- Sets an attribute to the desired amount");
+			msg(sender, "&7- &c/attrs reset &7- Resets all attributes to 0");
+			return true;
 		}
 		
 		return true;
 		
+	}
+	
+	public void msg(CommandSender p, String msg) {
+		p.sendMessage(msg.replaceAll("&", "§"));
+	}
+	public void msgP(CommandSender p, String msg) {
+		p.sendMessage("§4[§c§lMLMC§4] " + msg.replaceAll("&", "§"));
 	}
 }
