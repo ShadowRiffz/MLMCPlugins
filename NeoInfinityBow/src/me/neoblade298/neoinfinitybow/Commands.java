9bd93beb1d56fa728faf82ebdc142a7cfb15dc09
Alex Z <alexz1012@yahoo.com>
2019-02-25 20:02:54 -0800

    Init

---
+++ NeoInfinityBow/src/me/neoblade298/neoinfinitybow/Commands.java
@@ -0,0 +1,32 @@
+package me.neoblade298.neoinfinitybow;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+public class Commands implements CommandExecutor
+{
+  public Commands() {}
+  
+  public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args)
+  {
+    if ((args.length == 1) && 
+      (sender.isOp())) {
+      PlayerInventory inv = org.bukkit.Bukkit.getPlayer(args[0]).getInventory();
+      ItemStack item = inv.getItemInMainHand();
+      ItemStack offItem = inv.getItemInOffHand();
+      if (item.getEnchantmentLevel(Enchantment.ARROW_INFINITE) == 0) {
+    	  inv.removeItem(new ItemStack(Material.ARROW));
+      }
+      else if (offItem.getEnchantmentLevel(Enchantment.ARROW_INFINITE) == 0) {
+    	  inv.removeItem(new ItemStack(Material.ARROW));
+      }
+    }
+    
+    return true;
+  }
+}
---
+++ NeoInfinityBow/src/me/neoblade298/neoinfinitybow/Main.java
@@ -0,0 +1,24 @@
+package me.neoblade298.neoinfinitybow;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements org.bukkit.event.Listener
+{
+  public Main() {}
+  
+  public void onEnable()
+  {
+    super.onEnable();
+    org.bukkit.Bukkit.getServer().getLogger().info("NeoInfinityBow Enabled");
+    getServer().getPluginManager().registerEvents(this, this);
+    
+
+    getCommand("neoinfinitybow").setExecutor(new Commands());
+  }
+  
+  public void onDisable()
+  {
+    org.bukkit.Bukkit.getServer().getLogger().info("NeoChangelog Disabled");
+    super.onDisable();
+  }
+}
