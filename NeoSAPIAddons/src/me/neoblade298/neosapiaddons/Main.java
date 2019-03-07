22db7ae21bc25e82082fad1baa2cd15ad777a789
Alex Z <alexz1012@yahoo.com>
2019-03-06 23:27:28 -0800

    Remove unneeded imports

--- NeoSAPIAddons/src/me/neoblade298/neosapiaddons/Main.java
+++ NeoSAPIAddons/src/me/neoblade298/neosapiaddons/Main.java
@@ -2,11 +2,8 @@ package me.neoblade298.neosapiaddons;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.sucy.skill.api.event.SkillHealEvent;
