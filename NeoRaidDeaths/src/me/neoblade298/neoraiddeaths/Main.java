1ca7f764533f8bfe73b0b42cc078ca822496e38e
Alex Z <alexz1012@yahoo.com>
2019-03-06 23:20:29 -0800

    Add plugin yml

--- NeoRaidDeaths/src/me/neoblade298/neoraiddeaths/Main.java
+++ NeoRaidDeaths/src/me/neoblade298/neoraiddeaths/Main.java
@@ -25,6 +25,6 @@ public class Main
   
   @EventHandler
   public void onDeath(PlayerDeathEvent e) {
-	  e.getEntity().removeScoreboardTag("KaravRaidTag");
+	  e.getEntity().removeScoreboardTag("KaravRaidTPTag");
   }
 }
