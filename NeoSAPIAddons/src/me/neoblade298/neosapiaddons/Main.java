571803f00d6c3315463b5b62b21340e18ddf7eb4
Alex Z <alexz1012@yahoo.com>
2019-03-06 21:46:04 -0800

    Add curse implementation

--- NeoSAPIAddons/src/me/neoblade298/neosapiaddons/Main.java
+++ NeoSAPIAddons/src/me/neoblade298/neosapiaddons/Main.java
@@ -1,8 +1,16 @@
 package me.neoblade298.neosapiaddons;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sucy.skill.api.event.SkillHealEvent;
+import com.sucy.skill.api.util.FlagManager;
 
 public class Main
   extends JavaPlugin
@@ -21,5 +29,15 @@ public class Main
   {
     Bukkit.getServer().getLogger().info("NeoSAPIAddons Disabled");
     super.onDisable();
+  }
+  
+  // Implement curse status properly
+  @EventHandler
+  public void onHeal(SkillHealEvent e) {
+	  LivingEntity target = e.getTarget();
+	  if(FlagManager.hasFlag(target, "curse")) {
+		  e.setCancelled(true);
+		  target.damage(e.getAmount());
+	  }
   }
 }
