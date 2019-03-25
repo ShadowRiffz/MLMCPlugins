8ab30ad9f6cb5fb38d34e72f6d855cea75c41e52
Alex Z. <alexz1012@yahoo.com>
2019-03-25 00:52:54 -0700

    Remove unused method

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Utilities/CulinarianUtils.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Utilities/CulinarianUtils.java
@@ -86,25 +86,6 @@ public class CulinarianUtils {
 		return -1;
 	}
 	
-	public static int getFoodLevel(ItemStack item) {
-		if(item.hasItemMeta() && item.getItemMeta().hasLore()) {
-			String line = item.getItemMeta().getLore().get(0);
-			if(line.contains("Tier 1")) {
-				return 1;
-			}
-			else if(line.contains("Tier 2")) {
-				return 2;
-			}
-			else if(line.contains("Tier 3") || line.contains("Limited Edition")) {
-				return 3;
-			}
-			else if(line.contains("Legendary")) {
-				return 4;
-			}
-		}
-		return -1;
-	}
-	
 	public static int getTotalLeaves(ItemStack[] contents) {
 		int total = 0;
 		for(ItemStack content : contents) {
