e78562f762d894db4fc400b1d2d04a85ccc88143
Alex Z <alexz1012@yahoo.com>
2019-04-03 16:28:15 -0700

    Fix engrave cost

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Methods/MasonMethods.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Methods/MasonMethods.java
@@ -28,7 +28,7 @@ public class MasonMethods {
 	// Constants
 	static final int MAX_SLOTS = 3;
 	static final int ENGRAVE_ESSENCE_PER_SLOT = 12;
-	static final int ENGRAVE_GOLD_BASE = -10000;
+	static final int ENGRAVE_GOLD_BASE = 10000;
 	static final int ENGRAVE_GOLD_PER_LVL = 15000;
 	static final int UNENGRAVE_GOLD = 2000;
 	static final int UNENGRAVE_ESSENCE = 2;
@@ -91,10 +91,10 @@ public class MasonMethods {
 					if(numSlots < MAX_SLOTS) {
 						if (p.hasPermission(("mason.engrave.max."  + (numSlots + 1)))) {
 							if(p.getInventory().containsAtLeast(common.getEssence(level), ENGRAVE_ESSENCE_PER_SLOT * (numSlots + 1))) {
-								if(econ.has(p, ENGRAVE_GOLD_BASE + (ENGRAVE_GOLD_PER_LVL * level))) {
+								if(econ.has(p, ENGRAVE_GOLD_BASE + (ENGRAVE_GOLD_PER_LVL * numSlots))) {
 									masonUtils.createSlot(item, level);
 									p.getInventory().removeItem(util.setAmount(common.getEssence(level), ENGRAVE_ESSENCE_PER_SLOT * (numSlots + 1)));
-									econ.withdrawPlayer(p, ENGRAVE_GOLD_BASE + (ENGRAVE_GOLD_PER_LVL * level));
+									econ.withdrawPlayer(p, ENGRAVE_GOLD_BASE + (ENGRAVE_GOLD_PER_LVL * numSlots));
 									util.sendMessage(p, "&7Successfully created slot!");
 								}
 								else {
