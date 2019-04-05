4d531e49066a9dce26cdd501715081b6d3273791
Alex Z <alexz1012@yahoo.com>
2019-04-05 00:08:38 -0700

    Don't delete pprs on modify

--- NeoPPRs/src/me/neoblade298/neopprs/Commands.java
+++ NeoPPRs/src/me/neoblade298/neopprs/Commands.java
@@ -152,7 +152,6 @@ public class Commands implements CommandExecutor {
 							while (rs.next()) {
 								ppr = new PPR(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8));
 							}
-							stmt.executeUpdate("delete from neopprs_pprs WHERE id = " + id + ";");
 							con.close();
 						}
 						catch(Exception e) {
