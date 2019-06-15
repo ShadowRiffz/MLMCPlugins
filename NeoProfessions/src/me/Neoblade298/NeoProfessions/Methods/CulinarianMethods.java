4649f808f3ff558014571ccad97a25db1e3d3a0b
Alexander Zhou <alz030@ucsd.edu>
2019-06-14 23:51:42 -0700

    Reduce culinarian craft cost

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Methods/CulinarianMethods.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Methods/CulinarianMethods.java
@@ -54,7 +54,7 @@ public class CulinarianMethods {
 	final static int PRESERVE_ESSENCE = 1;
 	final static int SPICE_COST = 500;
 	final static int SPICE_ESSENCE = 1;
-	final static int CRAFT_COST = 50;
+	final static int CRAFT_COST = 5;
 	final static int ASSIMILATE_COST = 100;
 	final static int SPECIAL_COST = 5000;
 	final static int SPECIAL_AMOUNT = 16;
