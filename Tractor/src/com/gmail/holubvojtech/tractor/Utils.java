de9e60c1924b3cd9c61137bdb7ab0cce1d463810
Alex Z <alexz1012@yahoo.com>
2019-08-11 19:07:50 -0500

    Remove printlines

--- Tractor/src/com/gmail/holubvojtech/tractor/Utils.java
+++ Tractor/src/com/gmail/holubvojtech/tractor/Utils.java
@@ -4,14 +4,10 @@ import java.util.Map;
 
 public class Utils {
 	public static <K, V> V getOrDefault(Map<K, V> map, K key, V def) {
-		System.out.println("Start get or default");
 		V value = map.get(key);
-		System.out.println("value: " + value);
 		if (value == null) {
-			System.out.println("End get or default");
 			return def;
 		}
-		System.out.println("End get or default with value");
 		return value;
 	}
 }
