dcc7e1004f2b87807fd90ad1f6476455fe8647d2
Alex Z <alexz1012@yahoo.com>
2019-09-26 20:28:08 -0700

    Potentially fix and update to 1.14

--- OldEnchanting/src/im/hoss/OldEnchanting/OldEnchanting.java
+++ OldEnchanting/src/im/hoss/OldEnchanting/OldEnchanting.java
@@ -21,69 +21,59 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class OldEnchanting
-  extends JavaPlugin
-  implements Listener
-{
-  FileConfiguration config = getConfig();
-  
-  public void onEnable()
-  {
-    FileConfiguration config = getConfig();
-    config.addDefault("FreeLapis", Boolean.valueOf(true));
-    getServer().getPluginManager().registerEvents(this, this);
-    getLogger().info("OldEnchanting Enabled!");
-    
-    config.options().copyDefaults(true);
-    saveConfig();
-  }
-  
-  public void onDisable()
-  {
-    getLogger().info("OldEnchanting Disabled!");
-  }
-  
-  @EventHandler
-  public void onEnchant(EnchantItemEvent e)
-  {
-	  if (e.isCancelled()) {
-		  return;
-	  }
+public class OldEnchanting extends JavaPlugin implements Listener {
+	FileConfiguration config = getConfig();
+
+	public void onEnable() {
+		FileConfiguration config = getConfig();
+		config.addDefault("FreeLapis", Boolean.valueOf(true));
+		getServer().getPluginManager().registerEvents(this, this);
+		getLogger().info("OldEnchanting Enabled!");
+
+		config.options().copyDefaults(true);
+		saveConfig();
+	}
+
+	public void onDisable() {
+		getLogger().info("OldEnchanting Disabled!");
+	}
+
+	@EventHandler
+	public void onEnchant(EnchantItemEvent e) {
+		if (e.isCancelled()) {
+			return;
+		}
 		Map<Enchantment, Integer> enchs = e.getEnchantsToAdd();
 		if (enchs.containsKey(Enchantment.RIPTIDE) || enchs.containsKey(Enchantment.CHANNELING)) {
 			return;
 		}
-    e.getEnchanter().setLevel(e.getEnchanter().getLevel() - e.getExpLevelCost() + (e.whichButton() + 1));
-    e.setExpLevelCost(0);
-  }
-  
-  @EventHandler
-  public void onInventoryOpen(InventoryOpenEvent e)
-  {
-    if (e.getInventory().getType().equals(InventoryType.ENCHANTING)) {
-      if (this.config.getBoolean("FreeLapis"))
-      {
-        EnchantingInventory en = (EnchantingInventory)e.getInventory();
-        en.setSecondary(new ItemStack(Material.LAPIS_LAZULI, 64));
-      }
-    }
-  }
-  
-  @EventHandler
-  public void onInventoryClose(InventoryCloseEvent event)
-  {
-    if (event.getInventory().getType().equals(InventoryType.ENCHANTING)) {
-      event.getInventory().setItem(1, null);
-    }
-  }
-  
-  @EventHandler
-  public void onClick(InventoryClickEvent event)
-  {
-    if (event.getInventory().getType().equals(InventoryType.ENCHANTING)) {
-      if (event.getRawSlot() == 1) {
-        event.setCancelled(true);
-      }
-    }
-  }
+		e.getEnchanter().setLevel(e.getEnchanter().getLevel() - e.getExpLevelCost() + (e.whichButton() + 1) + 1);
+		e.setExpLevelCost(1);
+	}
+
+	@EventHandler
+	public void onInventoryOpen(InventoryOpenEvent e) {
+		if (e.getInventory().getType().equals(InventoryType.ENCHANTING)) {
+			if (this.config.getBoolean("FreeLapis")) {
+				EnchantingInventory en = (EnchantingInventory) e.getInventory();
+				en.setSecondary(new ItemStack(Material.LAPIS_LAZULI, 64));
+			}
+		}
+	}
+
+	@EventHandler
+	public void onInventoryClose(InventoryCloseEvent event) {
+		if (event.getInventory().getType().equals(InventoryType.ENCHANTING)) {
+			event.getInventory().setItem(1, null);
+		}
+	}
+
+	@EventHandler
+	public void onClick(InventoryClickEvent event) {
+		if (event.getInventory().getType().equals(InventoryType.ENCHANTING)) {
+			if (event.getRawSlot() == 1) {
+				event.setCancelled(true);
+			}
+		}
+	}
 }
