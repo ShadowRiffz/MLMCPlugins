c45d9bb644e0f76426eecabd25c9d7874a4da74a
Alexz1008 <alexz1012@yahoo.com>
2019-12-25 10:10:41 -0800

    Update base to have commands

--- Base/src/me/neoblade298/base/Main.java
+++ Base/src/me/neoblade298/base/Main.java
@@ -3,11 +3,14 @@ package me.neoblade298.base;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.neoblade298.neoattrsetter.Commands;
+
 public class Main extends JavaPlugin implements org.bukkit.event.Listener {
 	
 	public void onEnable() {
 		Bukkit.getServer().getLogger().info("NeoPPRs Enabled");
 		getServer().getPluginManager().registerEvents(this, this);
+	    this.getCommand("cmd").setExecutor(new Commands(this));
 	}
 	
 	public void onDisable() {
