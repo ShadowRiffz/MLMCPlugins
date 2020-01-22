906a52de46b6b1ca72cee49f11b0b68fbaaf352a
Alexander Zhou <alz030@ucsd.edu>
2020-01-22 08:29:24 -0800

    Add crossbow to infinity

--- NeoInfinityBow/src/me/neoblade298/neoinfinitybow/Commands.java
+++ NeoInfinityBow/src/me/neoblade298/neoinfinitybow/Commands.java
@@ -8,22 +8,21 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
-public class Commands implements CommandExecutor
-{
-  public Commands() {}
-  
-  public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args)
-  {
-    if ((args.length == 1) && 
-      (sender.isOp())) {
-      PlayerInventory inv = org.bukkit.Bukkit.getPlayer(args[0]).getInventory();
-      ItemStack item = inv.getItemInMainHand();
-      ItemStack offItem = inv.getItemInOffHand();
-      if (item.getEnchantmentLevel(Enchantment.ARROW_INFINITE) == 0 && offItem.getEnchantmentLevel(Enchantment.ARROW_INFINITE) == 0) {
-    	  inv.removeItem(new ItemStack(Material.ARROW));
-      }
-    }
-    
-    return true;
-  }
+public class Commands implements CommandExecutor {
+	public Commands() {
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+		if ((args.length == 1) && (sender.isOp())) {
+			PlayerInventory inv = org.bukkit.Bukkit.getPlayer(args[0]).getInventory();
+			ItemStack item = inv.getItemInMainHand();
+			ItemStack offItem = inv.getItemInOffHand();
+			if (item.getEnchantmentLevel(Enchantment.ARROW_INFINITE) == 0
+					&& offItem.getEnchantmentLevel(Enchantment.ARROW_INFINITE) == 0) {
+				inv.removeItem(new ItemStack(Material.ARROW));
+			}
+		}
+
+		return true;
+	}
 }
