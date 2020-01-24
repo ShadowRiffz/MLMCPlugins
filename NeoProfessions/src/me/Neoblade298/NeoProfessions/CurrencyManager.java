9cedc5bc83ff4feeb47ada779a9e9cb71af870b5
Alexz1008 <alexz1012@yahoo.com>
2020-01-23 16:43:00 -0800

    Complete SQL IO for essence

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/CurrencyManager.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/CurrencyManager.java
@@ -9,13 +9,10 @@ import java.util.UUID;
 
 import org.bukkit.entity.Player;
 
-import me.neoblade298.neoreports.Main;
-import me.neoblade298.neoreports.Report;
-
 public class CurrencyManager {
 	// UUID, essence/oretype, amount
-	public HashMap<UUID, HashMap<String, HashMap<Integer, Integer>>> currencies;
-	public static String[] types = {"essence", "ruby", "amethyst", "sapphire", "topaz", "garnet", "adamantium"};
+	private HashMap<UUID, HashMap<String, HashMap<Integer, Integer>>> currencies;
+	private static String[] types = {"essence", "ruby", "amethyst", "sapphire", "topaz", "garnet", "adamantium"};
 	
 	public CurrencyManager(Main main) {
 		currencies = new HashMap<UUID, HashMap<String, HashMap<Integer, Integer>>>();
@@ -36,23 +33,53 @@ public class CurrencyManager {
 			for (int i = 2; i <= 9; i++) {
 				String type = types[i-2];
 				HashMap<Integer, Integer> typeCurrencies = new HashMap<Integer, Integer>();
-				String result = rs.getString(i);
+				playerCurrencies.put(type, typeCurrencies);
+				String vals[] = rs.getString(i).split(":");
+				for (int j = 5; i <= 60; i += 5) {
+					typeCurrencies.put(j, Integer.parseInt(vals[i]));
+				}
 			}
 		}
 		else {
 			// User does not exist on sql
+			String init = "'0:0:0:0:0:0:0:0:0:0:0:0'";
+			for (int i = 1; i < types.length; i++) {
+				init += ", '0:0:0:0:0:0:0:0:0:0:0:0'";
+			}
+			int post = stmt.executeUpdate("INSERT INTO neoprofessions_currency "
+					+ "(`uuid`, `essence`, `ruby`, `amethyst`, `sapphire`, `emerald`, `topaz`, `garnet`, `adamantium`) "
+					+ "VALUES ('" + p.getUniqueId() + "', " + init + ");");
 			for (String type : types) {
 				HashMap<Integer, Integer> typeCurrencies = new HashMap<Integer, Integer>();
 				playerCurrencies.put(type, typeCurrencies);
-				for (int i = 5; i <= 60; i += 50) {
+				for (int i = 5; i <= 60; i += 5) {
 					typeCurrencies.put(i, 0);
 				}
+			}
+			if (post == 0) {
+				throw new Exception("Failed to post");
 			}
 		}
 		con.close();
 	}
 	
-	public void cleanup() {
-		
+	public void cleanup() throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection(Main.connection, Main.sqlUser, Main.sqlPass);
+		Statement stmt = con.createStatement();
+		for (UUID uuid : currencies.keySet()) {
+			HashMap<String, HashMap<Integer, Integer>> playerCurrencies = currencies.get(uuid);
+			for (String type : types) {
+				HashMap<Integer, Integer> typeCurrencies = playerCurrencies.get(type);
+				String sqlString = "" + typeCurrencies.get(5);
+				for (int i = 10; i <= 60; i += 5) {
+					sqlString = ":" + typeCurrencies.get(i);
+				}
+				int post = stmt.executeUpdate("UPDATE neoprofessions_currency SET '" + type + "' = '" + sqlString + "' WHERE uuid = '" + uuid + "');");
+				if (post == 0) {
+					throw new Exception("Failed to update");
+				}
+			}
+		}
 	}
 }
