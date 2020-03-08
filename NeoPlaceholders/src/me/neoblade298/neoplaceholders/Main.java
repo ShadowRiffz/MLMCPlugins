2d2746303c040fab21254d65b8782c3ce57d1248
Alexz1008 <alexz1012@yahoo.com>
2020-03-08 16:59:30 -0700

    Reformat

--- NeoPlaceholders/src/me/neoblade298/neoplaceholders/Main.java
+++ NeoPlaceholders/src/me/neoblade298/neoplaceholders/Main.java
@@ -1,19 +1,7 @@
 package me.neoblade298.neoplaceholders;
 
-import java.text.DecimalFormat;
-
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import com.sucy.skill.SkillAPI;
-import com.sucy.skill.api.classes.RPGClass;
-import com.sucy.skill.api.player.PlayerClass;
-import com.sucy.skill.api.player.PlayerData;
-
-import be.maximvdw.placeholderapi.PlaceholderAPI;
-import be.maximvdw.placeholderapi.PlaceholderReplaceEvent;
-import be.maximvdw.placeholderapi.PlaceholderReplacer;
 
 public class Main extends JavaPlugin implements org.bukkit.event.Listener {
 
@@ -22,6 +10,7 @@ public class Main extends JavaPlugin implements org.bukkit.event.Listener {
 		getServer().getPluginManager().registerEvents(this, this);
 		
 		new SkillAPIPlaceholders(this).registerPlaceholders();
+		new NeoprofessionsPlaceholders(this).registerPlaceholders();
 	}
 
 	public void onDisable() {
