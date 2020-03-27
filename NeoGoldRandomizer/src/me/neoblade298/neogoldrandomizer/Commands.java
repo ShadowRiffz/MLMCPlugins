6df816b4ab976e669093a1c96fb094e828809073
Alexz1008 <alexz1012@yahoo.com>
2020-03-26 20:44:50 -0700

    NeoBossInstances dependency and simplification

--- NeoGoldRandomizer/src/me/neoblade298/neogoldrandomizer/Commands.java
+++ NeoGoldRandomizer/src/me/neoblade298/neogoldrandomizer/Commands.java
@@ -1,17 +1,12 @@
 package me.neoblade298.neogoldrandomizer;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.UUID;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor {
 	Main main;
@@ -25,23 +20,7 @@ public class Commands implements CommandExecutor {
 			if (args.length == 3) {
 
 				// First check number of players in the instance
-				ArrayList<String> players = new ArrayList<String>();
-				try {
-					Connection con = DriverManager.getConnection(Main.connection, Main.sqlUser, Main.sqlPass);
-					Statement stmt = con.createStatement();
-					ResultSet rs;
-
-					// Find available instance
-					rs = stmt.executeQuery("SELECT * FROM neobossinstances_fights WHERE boss = '" + args[0]
-							+ "' AND instance = '" + Main.instanceName + "';");
-
-					while (rs.next()) {
-						players.add(Bukkit.getOfflinePlayer(UUID.fromString(rs.getString(1))).getName());
-						System.out.println(rs.getString(1));
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				ArrayList<Player> players = main.nbi.getActiveFights().get(args[0]);
 
 				// Get gold min and max for party size, generate gold
 				double bonus = 0.9 + (players.size() * 0.1);
@@ -51,11 +30,11 @@ public class Commands implements CommandExecutor {
 				gold = Math.round(gold / 25.0D) * 25L;
 				
 				// Give gold to each player
-				for (String player : players) {
+				for (Player player : players) {
 					ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
-					String command = "sync console MLMC eco give " + player + " " + (int) gold;
+					String command = "sync console MLMC eco give " + player.getName() + " " + (int) gold;
 					Bukkit.dispatchCommand(console, command);
-					Bukkit.getPlayer(player).sendMessage("§4[§c§lMLMC§4] §7You gained §e" + (int) gold + " §7gold!");
+					player.sendMessage("§4[§c§lMLMC§4] §7You gained §e" + (int) gold + " §7gold!");
 				}
 				return true;
 			}
--- NeoGoldRandomizer/src/me/neoblade298/neogoldrandomizer/Main.java
+++ NeoGoldRandomizer/src/me/neoblade298/neogoldrandomizer/Main.java
@@ -1,37 +1,18 @@
 package me.neoblade298.neogoldrandomizer;
 
-import java.io.File;
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
-	File file;
-	FileConfiguration conf;
-	static String sqlUser;
-	static String sqlPass;
-	static String connection;
-	static String instanceName;
+	protected me.neoblade298.neobossinstances.Main nbi;
 
 	public void onEnable() {
 		super.onEnable();
 		Bukkit.getServer().getLogger().info("NeoGoldRandomizer Enabled");
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
-
-		// Save config if doesn't exist
-		file = new File(getDataFolder(), "config.yml");
-		if (!file.exists()) {
-			saveResource("config.yml", false);
-		}
-		conf = YamlConfiguration.loadConfiguration(file);
-
-		// Load values from config
-		sqlUser = getConfig().getString("sql_user");
-		sqlPass = getConfig().getString("sql_pass");
-		connection = getConfig().getString("connection");
-		instanceName = getConfig().getString("instance_name");
+		
+		nbi = (me.neoblade298.neobossinstances.Main) Bukkit.getPluginManager().getPlugin("NeoBossInstances");
 
 		getCommand("neogoldrandomizer").setExecutor(new Commands(this));
 	}
