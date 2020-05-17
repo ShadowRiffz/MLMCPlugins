9e3f9dec8b934c83e6dd7251cfeecb2508e0ccde
Alexz1008 <alexz1012@yahoo.com>
2020-05-17 01:42:41 -0700

    Remove unecessary code

--- NeoMythicExtension/src/me/neoblade298/neomythicextension/Main.java
+++ NeoMythicExtension/src/me/neoblade298/neomythicextension/Main.java
@@ -7,7 +7,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicConditionLoadEvent;
 import io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMechanicLoadEvent;
