caf05666ca9f56fc56f7d480e63e16692675fd1b
Alexz1008 <alexz1012@yahoo.com>
2020-05-22 12:29:45 -0700

    Added way to give points to different class types

--- NeoSAPIAddons/src/me/neoblade298/neosapiaddons/Commands.java
+++ NeoSAPIAddons/src/me/neoblade298/neosapiaddons/Commands.java
@@ -10,30 +10,33 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class Commands
-  implements CommandExecutor
-{
-  public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args)
-  {
-    if ((sender.hasPermission("neosapiaddons.use")) && (args.length == 3))
-    {
-    	if (args[0].equalsIgnoreCase("takexp") && (StringUtils.isNumeric(args[2]))) {
-        double exp = Integer.parseInt(args[2]);
-        PlayerClass data = SkillAPI.getPlayerData(Bukkit.getPlayer(args[1])).getClass("class");
-        data.setExp(data.getExp() - exp);
-        sender.sendMessage("§4[§c§lMLMC§4] §c" + args[1] + " §7has §e" + data.getExp() + " §7exp remaining.");
-        return true;
-    	}
-    	
-    	else if (args[0].equalsIgnoreCase("skillup")) {
-    		Skill skill = SkillAPI.getSkill(args[2].replaceAll("_", " "));
-        SkillAPI.getPlayerData(Bukkit.getPlayer(args[1])).upgradeSkill(skill);
-        sender.sendMessage("§4[§c§lMLMC§4] §7Skill upgraded.");
-        return true;
-    	}
-    	
-    }
-    sender.sendMessage("Something went wrong.");
-    return true;
-  }
+public class Commands implements CommandExecutor {
+	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+		if ((sender.hasPermission("neosapiaddons.use")) && (args.length == 3)) {
+			if (args[0].equalsIgnoreCase("takexp") && (StringUtils.isNumeric(args[2]))) {
+				double exp = Integer.parseInt(args[2]);
+				PlayerClass data = SkillAPI.getPlayerData(Bukkit.getPlayer(args[1])).getClass("class");
+				data.setExp(data.getExp() - exp);
+				sender.sendMessage("§4[§c§lMLMC§4] §c" + args[1] + " §7has §e" + data.getExp() + " §7exp remaining.");
+				return true;
+			}
+
+			else if (args[0].equalsIgnoreCase("skillup")) {
+				Skill skill = SkillAPI.getSkill(args[2].replaceAll("_", " "));
+				SkillAPI.getPlayerData(Bukkit.getPlayer(args[1])).upgradeSkill(skill);
+				sender.sendMessage("§4[§c§lMLMC§4] §7Skill upgraded.");
+				return true;
+			}
+
+			// neosapiaddons points [player] [classtype] [amount]
+			else if (args[0].equalsIgnoreCase("points")) {
+				SkillAPI.getPlayerData(Bukkit.getPlayer(args[1])).getClass(args[2]).givePoints(Integer.parseInt(args[3]));
+				sender.sendMessage("§4[§c§lMLMC§4] §7Points given.");
+				return true;
+			}
+
+		}
+		sender.sendMessage("Something went wrong.");
+		return true;
+	}
 }
