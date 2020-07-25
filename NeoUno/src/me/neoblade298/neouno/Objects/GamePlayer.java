a4b06f79841c51faec859017cfd6f1c8e24d6380
Alexz1008 <alexz1012@yahoo.com>
2020-07-24 22:35:58 -0700

    add bold to your turn

--- NeoUno/src/me/neoblade298/neouno/Objects/GamePlayer.java
+++ NeoUno/src/me/neoblade298/neouno/Objects/GamePlayer.java
@@ -33,7 +33,7 @@ public class GamePlayer {
 	
 	public void showHandOld() {
 		if (game.curr.equals(this)) {
-			message("Your turn! Top card is: " + game.topCard.getDisplay());
+			message("&lYour turn! &7Top card is: " + game.topCard.getDisplay());
 		}
 		else {
 			message("&f" + game.curr + "'s &7turn! Top card is: " + game.topCard.getDisplay());
