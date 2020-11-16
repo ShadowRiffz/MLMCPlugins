3b08f9303561cf701511bfb68f83cb6d7f75991b
Alexz1008 <alexz1012@yahoo.com>
2020-11-15 22:51:58 -0800

    Add refresh command

--- NeoSAPIAddons/src/me/neoblade298/neosapiaddons/Commands.java
+++ NeoSAPIAddons/src/me/neoblade298/neosapiaddons/Commands.java
@@ -9,10 +9,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class Commands implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
 		if (sender.hasPermission("neosapiaddons.use")) {
+			if (args.length == 0) {
+				sender.sendMessage("§c/neosapiaddons takexp [player] [exp]");
+				sender.sendMessage("§c/neosapiaddons skillup [player] [skill]");
+				sender.sendMessage("§c/neosapiaddons points [player] [classtype] [amount]");
+				sender.sendMessage("§c/neosapiaddons refresh [player]");
+			}
 			if (args[0].equalsIgnoreCase("takexp") && (StringUtils.isNumeric(args[2]))) {
 				double exp = Integer.parseInt(args[2]);
 				PlayerClass data = SkillAPI.getPlayerData(Bukkit.getPlayer(args[1])).getClass("class");
@@ -33,6 +40,11 @@ public class Commands implements CommandExecutor {
 				SkillAPI.getPlayerData(Bukkit.getPlayer(args[1])).getClass(args[2]).givePoints(Integer.parseInt(args[3]));
 				sender.sendMessage("§4[§c§lMLMC§4] §7Points given.");
 				return true;
+			}
+			else if (args[0].equalsIgnoreCase("refresh")) {
+	            Player p = Bukkit.getPlayer(args[1]);
+                SkillAPI.getPlayerData(p).getEquips().update(p);
+                return true;
 			}
 
 		}
