a35d3df616dcd6ebb8596d9b123889629025f636
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2021-08-25 23:21:30 -0700

    Add stuff to test

--- ETC/src/MoneyCalculator.java
+++ ETC/src/MoneyCalculator.java
@@ -1,43 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class MoneyCalculator {
 	public static void main (String[] args) {
-		File file = new File("C:\\Users\\Alex\\Desktop\\bug.txt");
-		HashMap<String, Integer> players = new HashMap<String, Integer>();
+		File file = new File("C:\\Users\\Alex\\Desktop\\bugged acc.txt");
+		String account = Files.readAllLines(file.toPath());
 		try {
 			Scanner scan = new Scanner(file);
 			while (scan.hasNext()) {
-				String line = scan.nextLine();
-				String[] arguments = line.split(" ");
-				if (arguments[4].contains("/eco")) {
-					String player = arguments[6];
-					int amount = Integer.parseInt(arguments[7]);
-					if (players.containsKey(player)) {
-						int newTotal = players.get(player) + amount;
-						players.put(player, newTotal);
-					}
-					else {
-						players.put(player, amount);
-					}
+				account += scan.nextLine();
+				if (scan.hasNext()) {
+					account += "\n";
 				}
+				
 			}
 			scan.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		int count = 0;
-		System.out.println();
-		for (String p : players.keySet()) {
-			System.out.println("  - /eco take " + p + " " + players.get(p));
-			count++;
-		}
-		System.out.println("Total players: " + count);
-		
-		
+		System.out.println(account);
+		DataSection data = YAMLParser.parseText(account);
+		System.out.println(data);
 	}
 }
