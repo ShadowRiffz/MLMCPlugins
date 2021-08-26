5d8e9ef752e51783667957b8d5a2e321e24e109a
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2021-08-26 13:45:40 -0700

    Changes

--- ETC/src/MoneyCalculator.java
+++ ETC/src/MoneyCalculator.java
@@ -1,13 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class MoneyCalculator {
 	public static void main (String[] args) {
-		File file = new File("C:\\Users\\Alex\\Desktop\\bugged acc.txt");
-		String account = Files.readAllLines(file.toPath());
+		File file = new File("C:\\Users\\Alex\\Desktop\\error 2.txt");
+		String account = new String();
 		try {
 			Scanner scan = new Scanner(file);
 			while (scan.hasNext()) {
@@ -24,6 +23,8 @@ public class MoneyCalculator {
 		}
 		System.out.println(account);
 		DataSection data = YAMLParser.parseText(account);
-		System.out.println(data);
+		System.out.println("Stop here");
+		System.out.println(data.getDouble("health"));
+		System.out.println(data.get)
 	}
 }
