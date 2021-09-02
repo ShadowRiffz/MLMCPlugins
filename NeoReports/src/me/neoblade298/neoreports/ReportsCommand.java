d546299814b3b083a137e012dc317344d717b79b
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2021-09-01 17:58:06 -0700

    Remove unnecessary import

--- NeoReports/src/me/neoblade298/neoreports/ReportsCommand.java
+++ NeoReports/src/me/neoblade298/neoreports/ReportsCommand.java
@@ -13,7 +13,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import me.neoblade298.neoreports.Main;
 
