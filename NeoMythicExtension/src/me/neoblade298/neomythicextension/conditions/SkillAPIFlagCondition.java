c1b01cd294ddd3758c97757d668c595811fa206b
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2021-09-26 21:20:21 -0700

    Add player support for flagcondition

--- NeoMythicExtension/src/me/neoblade298/neomythicextension/conditions/SkillAPIFlagCondition.java
+++ NeoMythicExtension/src/me/neoblade298/neomythicextension/conditions/SkillAPIFlagCondition.java
@@ -17,98 +17,143 @@ import io.lumine.xikage.mythicmobs.skills.conditions.IEntityCondition;
 import me.neoblade298.neomythicextension.Main;
 
 public class SkillAPIFlagCondition extends SkillCondition implements IEntityCondition {
-    private String[] flags;
-    private boolean castinstead = false;
-    private boolean stunchildren = false;
-    private boolean action = true;
-    private int setgcd = -1;
-    private String msg;
-    private ArrayList<Entity> near;
-    private Main main;
-    
-    public SkillAPIFlagCondition(MythicLineConfig mlc, Main main) {
-        super(mlc.getLine());
-        this.flags = mlc.getString(new String[] {"flag", "f"}).trim().split(",");
-        if(mlc.getString("action") != null) {
-        	castinstead = mlc.getString("action").equals("castinstead");
-        	action = mlc.getString("action").equals("true");
-        }
-        if(mlc.getString("stunchildren") != null) {
-        	stunchildren = mlc.getString("stunchildren").equals("true");
-        }
-        if(mlc.getInteger("setgcd") != 0) {
-        	setgcd = mlc.getInteger("setgcd");
-        }
-        msg = mlc.getString("msg");
-        near = null;
-        this.main = main;
-    }
+	private String[] flags;
+	private boolean castinstead = false;
+	private boolean stunchildren = false;
+	private boolean action = true;
+	private int setgcd = -1;
+	private String msg;
+	private ArrayList<Entity> near;
+	private Main main;
 
-    public boolean check(AbstractEntity t) {
-        ActiveMob am = MythicMobs.inst().getMobManager().getMythicMobInstance(t);
-        boolean result = false;
-        if(am != null) {
-	        if(msg != null) {
-	        	if(am.getEntity().getName() != null) {
-	        		msg = msg.replace("<mob.name>", am.getEntity().getName());
-	        	}
-				msg = msg.replace("&", "§");
-				msg = msg.replace("_", " ");
-	        }
-	        if (am != null) {
-	        	LivingEntity ent = (LivingEntity) am.getEntity().getBukkitEntity();
-	        	for (String flag : flags) {
-	        		if (FlagManager.hasFlag(ent, flag)) {
-	        			
-	        			// Very specific behavior for stun rework, only use this when castinstead is enabled
-	        	    	if (castinstead) {
-	        	    		// Give the entity a stun tag
-		    				if(!am.getEntity().hasScoreboardTag("StunTag")) {
-		        	    		am.getEntity().addScoreboardTag("StunTag");
-		        	    		
-		        	    		// If a message was specified, show players in radius the message
-		            	    	if(msg != null) {
-		            	    		Bukkit.getScheduler().scheduleSyncDelayedTask(main, new Runnable() {
-		            	    			public void run() {
-				            	    		near = (ArrayList<Entity>) am.getEntity().getBukkitEntity().getNearbyEntities(40, 40, 40);
-				            	    		displayMessage();
-		            	    			}
-		            	    		});
-	            	    		}
-	            	    	}
-		    				if (setgcd != -1) {
-		    					am.setGlobalCooldown(setgcd);
-		    				}
-		    				
-		    				// If stun children, iterate through each child and also give them a stun tag
-		    				if(stunchildren) {
-		    					for (AbstractEntity e : am.getChildren()) {
-		    						e.getBukkitEntity().addScoreboardTag("StunTag");
-		    					}
-		    				}
-		    				
-	        	    	}
-	        			result = true;
-	        		}
-	        		else {
-	        			result = false;
-	        			break;
-	        		}
-	        	}
-	        }
-	        if(action && !castinstead) {
-	        	result = !result;
-	        }
-        }
-        return !result;
-    }
-    
-    public void displayMessage() {
-		for(Entity e : near) {
+	public SkillAPIFlagCondition(MythicLineConfig mlc, Main main) {
+		super(mlc.getLine());
+		this.flags = mlc.getString(new String[] { "flag", "f" }).trim().split(",");
+		if (mlc.getString("action") != null) {
+			castinstead = mlc.getString("action").equals("castinstead");
+
+			// Make it so action is true by default
+			action = !mlc.getString("action").equals("false");
+		}
+		if (mlc.getString("stunchildren") != null) {
+			stunchildren = mlc.getString("stunchildren").equals("true");
+		}
+		if (mlc.getInteger("setgcd") != 0) {
+			setgcd = mlc.getInteger("setgcd");
+		}
+		msg = mlc.getString("msg");
+		near = null;
+		this.main = main;
+	}
+
+	public boolean check(AbstractEntity t) {
+		ActiveMob am = MythicMobs.inst().getMobManager().getMythicMobInstance(t);
+		boolean result = false;
+		
+		// Checking a player for a flag
+		if (t.getBukkitEntity() instanceof Player) {
+			Player p = (Player) t.getBukkitEntity();
+			result = checkPlayer(p);
+		}
+		else if (am != null) {
+			result = checkMob((LivingEntity) t.getBukkitEntity(), am);
+		}
+		return result;
+	}
+	
+	private boolean checkPlayer(Player p) {
+		boolean result = false;
+		if (action) {
+			for (String flag : flags) {
+				if (FlagManager.hasFlag(p, flag)) {
+					result = true;
+				}
+				else {
+					result = false;
+					break;
+				}
+			}
+		}
+		else {
+			for (String flag : flags) {
+				if (!FlagManager.hasFlag(p, flag)) {
+					result = true;
+				}
+				else {
+					result = false;
+					break;
+				}
+			}
+		}
+		return result;
+	}
+	
+	private boolean checkMob(LivingEntity ent, ActiveMob am) {
+		boolean result = true;
+		if (action) {
+			for (String flag : flags) {
+				if (FlagManager.hasFlag(ent, flag)) {
+					result = true;
+	
+					// Very specific behavior for stun rework
+					if (castinstead) {
+						// Give the entity a stun tag
+						if (!am.getEntity().hasScoreboardTag("StunTag")) {
+							am.getEntity().addScoreboardTag("StunTag");
+	
+							// If a message was specified, show players in radius the message
+							if (msg != null) {
+								if (am.getEntity().getName() != null) {
+									msg = msg.replace("<mob.name>", am.getEntity().getName());
+								}
+								msg = msg.replace("&", "§");
+								msg = msg.replace("_", " ");
+								Bukkit.getScheduler().scheduleSyncDelayedTask(main, new Runnable() {
+									public void run() {
+										near = (ArrayList<Entity>) am.getEntity().getBukkitEntity()
+												.getNearbyEntities(40, 40, 40);
+										displayMessage();
+									}
+								});
+							}
+						}
+						if (setgcd != -1) {
+							am.setGlobalCooldown(setgcd);
+						}
+	
+						// If stun children, iterate through each child and also give them a stun tag
+						if (stunchildren) {
+							for (AbstractEntity e : am.getChildren()) {
+								e.getBukkitEntity().addScoreboardTag("StunTag");
+							}
+						}
+	
+					}
+				}
+				else {
+					result = false;
+					break;
+				}
+			}
+		}
+		else {
+			for (String flag : flags) {
+				if (FlagManager.hasFlag(ent, flag)) {
+					// If any flag exists, result is false.
+					result = false;
+					break;
+				}
+			}
+		}
+		return result;
+	}
+
+	public void displayMessage() {
+		for (Entity e : near) {
 			if (e instanceof Player) {
 				Player p = (Player) e;
 				p.sendMessage(msg);
 			}
 		}
-    }
+	}
 }
