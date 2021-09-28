4e451d867f762cffd94171f251483b0f7a657e05
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2021-09-27 22:56:24 -0700

    Update spawnmythicmob with level

--- NeoSAPIAddons/src/me/neoblade298/neosapiaddons/mechanics/SpawnMythicmobMechanic.java
+++ NeoSAPIAddons/src/me/neoblade298/neosapiaddons/mechanics/SpawnMythicmobMechanic.java
@@ -1,14 +1,20 @@
 package me.neoblade298.neosapiaddons.mechanics;
 
+import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.entity.LivingEntity;
+
 import com.google.common.collect.ImmutableList;
 import com.sucy.skill.dynamic.ComponentType;
+import com.sucy.skill.dynamic.DynamicSkill;
 import com.sucy.skill.dynamic.custom.CustomEffectComponent;
 import com.sucy.skill.dynamic.custom.EditorOption;
 
 import io.lumine.xikage.mythicmobs.MythicMobs;
+import io.lumine.xikage.mythicmobs.mobs.ActiveMob;
+import io.lumine.xikage.mythicmobs.mobs.MobManager;
 
 public class SpawnMythicmobMechanic extends CustomEffectComponent {
 
@@ -29,19 +35,37 @@ public class SpawnMythicmobMechanic extends CustomEffectComponent {
                         "target",
                         "Target",
                         "Who to target",
-                        ImmutableList.of("caster", "target"))
+                        ImmutableList.of("caster", "target")),
+                EditorOption.text(
+                        "level",
+                        "Level",
+                        "The key to use for the level of the mob, or override with int",
+                        "max_val")
         );
 	}
 
 	@Override
 	public boolean execute(LivingEntity caster, int lvl, List<LivingEntity> targets) {
 		String mob = settings.getString("mob");
+		String levelKey = settings.getString("level");
+		HashMap<String, Object> data = DynamicSkill.getCastData(caster);
+		int moblevel = 1;
+		if (StringUtils.isNumeric(levelKey)) {
+			moblevel = Integer.parseInt(levelKey);
+		}
+		else {
+			moblevel = (int) ((double) data.get(levelKey));
+		}
+		MobManager mm = MythicMobs.inst().getMobManager();
+		
 		if (settings.getString("target").equalsIgnoreCase("caster")) {
-			MythicMobs.inst().getMobManager().spawnMob(mob, caster.getLocation());
+			ActiveMob am = mm.spawnMob(mob, caster.getLocation(), moblevel);
+			am.setOwner(caster.getUniqueId());
 		}
 		else {
 			for (LivingEntity ent : targets) {
-				MythicMobs.inst().getMobManager().spawnMob(mob, ent.getLocation());
+				ActiveMob am = mm.spawnMob(mob, ent.getLocation(), moblevel);
+				am.setOwner(caster.getUniqueId());
 			}
 		}
 		return true;
