2265a020e611ee8d01883a2df45356a593055f3a
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2021-11-14 21:09:32 -0800

    Work on augments

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Augments/PlayerAugments.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Augments/PlayerAugments.java
@@ -1,5 +1,71 @@
 package me.Neoblade298.NeoProfessions.Augments;
 
-public class PlayerAugments {
+import java.util.ArrayList;
 
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+import de.tr7zw.nbtapi.NBTItem;
+
+public class PlayerAugments {
+	private ArrayList<Augment> chestAugs;
+	private ArrayList<Augment> legsAugs;
+	private ArrayList<Augment> mainAugs;
+	private ArrayList<Augment> offAugs;
+	private Player p;
+	
+	private final static int MAX_SLOTS = 5;
+	
+	public PlayerAugments(Player p) {
+		this.p = p;
+		this.chestAugs = new ArrayList<Augment>();
+		this.legsAugs = new ArrayList<Augment>();
+		this.mainAugs = new ArrayList<Augment>();
+		this.offAugs = new ArrayList<Augment>();
+	}
+	
+	public void swapHands() {
+		ArrayList<Augment> temp = this.mainAugs;
+		this.mainAugs = this.offAugs;
+		this.mainAugs = temp;
+	}
+	
+	public void recalculateMainhand() {
+		this.mainAugs = checkAugments(p.getInventory().getItemInMainHand());
+	}
+	
+	public void recalculateAll() {
+		PlayerInventory inv = p.getInventory();
+		this.chestAugs = checkAugments(inv.getChestplate());
+		this.legsAugs = checkAugments(inv.getLeggings());
+		this.mainAugs = checkAugments(inv.getItemInMainHand());
+		this.offAugs = checkAugments(inv.getItemInOffHand());
+	}
+	
+	public ArrayList<Augment> checkAugments(ItemStack item) {
+		ArrayList<Augment> augs = new ArrayList<Augment>();
+		
+		if (item != null && !item.getType().isAir()) {
+			NBTItem nbti = new NBTItem(item);
+			for (int i = 1; i <= MAX_SLOTS; i++) {
+				String augmentName = nbti.getString("slot" + i + "Augment");
+				if (AugmentManager.nameMap.containsKey(augmentName)) {
+					Augment aug = AugmentManager.nameMap.get(augmentName);
+					augs.add(aug);
+				}
+			}
+		}
+		
+		return augs;
+	}
+	
+	public ArrayList<Augment> getAugments() {
+		ArrayList<Augment> augments = new ArrayList<Augment>();
+		augments.addAll(chestAugs);
+		augments.addAll(legsAugs);
+		augments.addAll(mainAugs);
+		augments.addAll(offAugs);
+		return augments;
+	}
 }
