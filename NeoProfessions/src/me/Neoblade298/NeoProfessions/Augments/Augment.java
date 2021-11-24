8d8cbe3fffb28f16a0f8ef97627b337330f10c7d
Alex Z <alexz1012@yahoo.com>
2021-11-24 09:41:18 -0800

    Simplify getItem

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Augments/Augment.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Augments/Augment.java
@@ -1,6 +1,12 @@
 package me.Neoblade298.NeoProfessions.Augments;
 
+import java.util.ArrayList;
+
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import de.tr7zw.nbtapi.NBTItem;
 
 public abstract class Augment {
 	protected String name;
@@ -35,5 +41,19 @@ public abstract class Augment {
 	}
 	
 	public abstract Augment createNew(int level);
-	public abstract ItemStack getItem();
+	
+	public ItemStack getItem() {
+		ItemStack item = new ItemStack(Material.ENDER_PEARL);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName("§4[Lv " + level + "] §c" + name + " Augment");
+		ArrayList<String> lore = new ArrayList<String>();
+		lore.add("§7Level " + level + " " + name + " Augment");
+		meta.setLore(lore);
+		item.setItemMeta(meta);
+		NBTItem nbti = new NBTItem(item);
+		nbti.setInteger("level", level);
+		nbti.setString("augment", name);
+		return nbti.getItem();
+	}
+
 }
