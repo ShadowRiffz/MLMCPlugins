57866de18d5157ef8c0b6467c4df5795efa19065
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-02-12 11:59:41 -0800

    Standardize essence gain

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/CurrencyManager.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/CurrencyManager.java
@@ -99,13 +99,17 @@ public class CurrencyManager {
 	}
 	
 	public void add(Player p, String type, int level, int amount) {
+		if (level > 60 || level < 5) {
+			return;
+		}
+
+		// Standardize the level
+		level -= (level % 5);
 		if (amount < 0) {
 			subtract(p, type, level, -amount);
 			return;
 		}
-		if (!(level <= 60 && level > 0 && level % 5 == 0)) {
-			return;
-		}
+		
 		HashMap<Integer, Integer> typeCurrency = currencies.get(p.getUniqueId()).get(type.toLowerCase());
 		int newAmount = typeCurrency.get(level) + amount;
 		typeCurrency.put(level, newAmount);
@@ -113,9 +117,6 @@ public class CurrencyManager {
 	}
 	
 	public void subtract(Player p, String type, int level, int amount) {
-		if (!(level <= 60 && level > 0 && level % 5 == 0)) {
-			return;
-		}
 		HashMap<Integer, Integer> typeCurrency = currencies.get(p.getUniqueId()).get(type.toLowerCase());
 		int newAmount = typeCurrency.get(level) - amount;
 		typeCurrency.put(level, newAmount);
