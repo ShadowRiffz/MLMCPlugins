07a551f6bfc3823ddc56a285df8204b7bf45d515
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-03-10 16:06:20 -0800

    Clean more

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Legacy/Items/StonecutterItems.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Legacy/Items/StonecutterItems.java
@@ -10,26 +10,6 @@ import org.bukkit.inventory.ItemStack;
 public class StonecutterItems {
 	
 	// Constants
-	final int WEAPON_GEM_STR_BASE = 4;
-	final int WEAPON_GEM_STR_PER_LVL = 2;
-	final int ARMOR_GEM_STR_PER_LVL = 1;
-	final int ARMOR_GEM_STR_BASE = 2;
-	final int ARMOR_GEM_VIT_PER_LVL = 7;
-	final int ARMOR_GEM_VIT_BASE = 1;
-	final int ARMOR_GEM_END_PER_LVL = 4;
-	final int ARMOR_GEM_END_BASE = 2;
-	final int OL_WEAPON_GEM_STR_RANGE = 3;
-	final int OL_ARMOR_GEM_STR_RANGE = 2;
-	final int OL_ARMOR_GEM_STR_BASE = 1;
-	final int OL_ARMOR_GEM_STR_PER_LVL = 2;
-	final int OL_ARMOR_GEM_VIT_RANGE = 15;
-	final int OL_ARMOR_GEM_VIT_BASE = 20; // set to 200 max or something
-	final int OL_ARMOR_GEM_VIT_PER_LVL = 15;
-	final int OL_ARMOR_GEM_END_RANGE = 3;
-	final int OL_ARMOR_GEM_END_BASE = 1;
-	final int OL_ARMOR_GEM_END_PER_LVL = 3;
-	final int OL_WEAPON_DURABILITY_BASE = 400;
-	final int OL_ARMOR_DURABILITY_BASE = 200;
 	
 	Random gen = new Random();
 	
