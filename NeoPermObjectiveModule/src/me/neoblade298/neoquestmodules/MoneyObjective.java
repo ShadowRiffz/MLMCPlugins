9ab788c49d2c265e51b5c0370630db66520d4df9
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-03-21 12:50:00 -0700

    Group quest modules together, add money module

---
+++ NeoPermObjectiveModule/src/me/neoblade298/neoquestmodules/MoneyObjective.java
@@ -0,0 +1,39 @@
+package me.neoblade298.neoquestmodules;
+
+import me.blackvein.quests.CustomObjective;
+import me.blackvein.quests.Quest;
+import me.blackvein.quests.Quests;
+import net.ess3.api.events.UserBalanceUpdateEvent;
+
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class MoneyObjective extends CustomObjective implements Listener {
+    // Get the Quests plugin
+    Quests qp = (Quests)Bukkit.getServer().getPluginManager().getPlugin("Quests");
+	
+    // Construct the objective
+    public MoneyObjective() {
+        this.setName("Money Objective");
+        this.setAuthor("Neoblade298");
+        this.addStringPrompt("Amount", "Enter the amount of money to reach", "5000");
+    }
+
+    // Catch the Bukkit event for a player gaining/losing exp
+    @EventHandler
+    public void onBalanceUpdate (UserBalanceUpdateEvent e){
+    	Player p = e.getPlayer();
+    	double bal = e.getNewBalance().doubleValue();
+
+    	for (Quest quest : qp.getQuester(e.getPlayer().getUniqueId()).getCurrentQuests().keySet()) {
+    	    Map<String, Object> map = getDataForPlayer(p, this, quest);
+	    	if (bal >= Integer.parseInt((String) map.get("Amount"))) {
+				incrementObjective(e.getPlayer(), this, 1, quest);
+	    	}
+    	}
+    }
+}
