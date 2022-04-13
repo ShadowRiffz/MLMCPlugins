360f406e86c97341343cd736a4355f261fb3613c
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-04-12 23:55:07 -0700

    Fix fertilizer level

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Inventories/GardenChooseFertilizerView.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Inventories/GardenChooseFertilizerView.java
@@ -113,7 +113,7 @@ public class GardenChooseFertilizerView extends ProfessionInventory {
 		int seedLevel = StorageManager.getItem(id).getLevel();
 		
 		// Make sure the level of the fertilizer is high enough
-		if (fertilizerId == -1 || StorageManager.getItem(fertilizerId).getLevel() < seedLevel) {
+		if (fertilizerId != -1 && StorageManager.getItem(fertilizerId).getLevel() < seedLevel) {
 			p.sendMessage("§cThis fertilizer is not high enough level!");
 			p.playSound(p.getLocation(), Sound.BLOCK_NOTE_BLOCK_BASS, 1.0F, HarvestingMinigame.ERROR);
 			return;
