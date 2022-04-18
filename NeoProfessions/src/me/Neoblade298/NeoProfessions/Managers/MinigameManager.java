23c2fabed530ae86050b1fa1bf6aa2477b9e2bca
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-04-17 17:50:41 -0700

    Properly import listener

--- NeoProfessions/src/me/Neoblade298/NeoProfessions/Managers/MinigameManager.java
+++ NeoProfessions/src/me/Neoblade298/NeoProfessions/Managers/MinigameManager.java
@@ -9,6 +9,7 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 import me.Neoblade298.NeoProfessions.Professions;
