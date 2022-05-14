a881a15d4eb86c5474c2b43b3a7275afab67b56b
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-05-14 10:19:54 -0700

    Start work on neoquests

---
+++ NeoQuests/src/me/neoblade298/neoquests/quests/QuestsManager.java
@@ -0,0 +1,48 @@
+package me.neoblade298.neoquests.quests;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+import me.neoblade298.neoquests.io.IOComponent;
+
+public class QuestsManager implements IOComponent {
+	private HashMap<Player, Quester> questers;
+
+	
+	@Override
+	public void loadPlayer(OfflinePlayer p, Statement stmt) {
+		try {
+			ResultSet rs = stmt.executeQuery("SELECT * FROM quests_users WHERE UUID = '" + p.getUniqueId() + "';");
+			while (rs.next()) {
+				// TODO: Set up user
+			}
+		}
+		catch (Exception e) {
+			Bukkit.getLogger().log(Level.WARNING, "Quests failed to load or init quest data for user " + p.getName());
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void savePlayer(Player p, Statement stmt) {
+		try {
+			Quester quester = questers.get(p);
+			// TODO: Save user
+			stmt.addBatch("REPLACE INTO quests_quests "
+					+ "VALUES ()");
+		}
+		catch (Exception e) {
+			Bukkit.getLogger().log(Level.WARNING, "Quests failed to save quest data for user " + p.getName());
+			e.printStackTrace();
+		}
+	}
+}
