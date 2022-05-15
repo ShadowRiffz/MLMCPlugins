f49bbdc17f1e12dbbf61665a9c192fcbc9804e4a
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-05-15 13:14:58 -0700

    Add neocore dependant system

---
+++ NeoCore/src/me/neoblade298/neocore/Dependant.java
@@ -0,0 +1,5 @@
+package me.neoblade298.neocore;
+
+public interface Dependant {
+	public void run();
+}
--- NeoCore/src/me/neoblade298/neocore/NeoCore.java
+++ NeoCore/src/me/neoblade298/neocore/NeoCore.java
@@ -1,21 +1,26 @@
 package me.neoblade298.neocore;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Properties;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import me.neoblade298.neocore.events.NeoCoreInitEvent;
+import me.neoblade298.neocore.events.NeoPluginLoadEvent;
 import me.neoblade298.neocore.listeners.IOListener;
 
 public class NeoCore extends JavaPlugin implements org.bukkit.event.Listener {
 	private static NeoCore inst;
 	public static String sqlUser, sqlPass, connection;
 	public static Properties properties = new Properties();
+	private static HashMap<String, ArrayList<Dependant>> dependants = new HashMap<String, ArrayList<Dependant>>();
 	
 	public void onEnable() {
 		inst = this;
@@ -48,6 +53,22 @@ public class NeoCore extends JavaPlugin implements org.bukkit.event.Listener {
 	
 	public static NeoCore inst() {
 		return inst;
+	}
+	
+	public static void addDependant(String msg, Dependant d) {
+		ArrayList<Dependant> list = dependants.getOrDefault(msg, new ArrayList<Dependant>());
+		list.add(d);
+		dependants.put(msg, list);
+	}
+	
+	@EventHandler
+	public void onDependableLoad(NeoPluginLoadEvent e) {
+		String msg = e.getMessage();
+		if (dependants.containsKey(msg)) {
+			for (Dependant dep : dependants.get(msg)) {
+				dep.run();
+			}
+		}
 	}
 	
 }
