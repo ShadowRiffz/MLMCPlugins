722aac722a5049946acde28aa2f4ada933ff14ee
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-05-19 20:51:09 -0700

    Modify shard name

--- NeoGear/src/me/neoblade298/neogear/objects/Shards.java
+++ NeoGear/src/me/neoblade298/neogear/objects/Shards.java
@@ -9,12 +9,13 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import de.tr7zw.nbtapi.NBTItem;
+import net.md_5.bungee.api.ChatColor;
 
 public class Shards {
 	public static ItemStack getRarityShard(Rarity rarityMax, int levelMax) {
 		ItemStack item = new ItemStack(Material.AMETHYST_SHARD);
 		ItemMeta meta = item.getItemMeta();
-		meta.setDisplayName("§6[" + levelMax + "] §cRarity Upgrade Shard");
+		meta.setDisplayName("§6[" + levelMax + "] §c" + ChatColor.stripColor(rarityMax.displayName) + " Upgrade Shard");
 		List<String> lore = new ArrayList<String>();
 		lore.add("§7Increases rarity of quest equipment.");
 		lore.add("§7Works on up to Lv " + levelMax + " " + rarityMax.displayName + " §7gear");
