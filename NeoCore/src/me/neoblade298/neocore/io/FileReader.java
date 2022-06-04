bacf1bd787307b600eb9a4cecfaffcfd71ce95e2
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-06-04 11:08:55 -0700

    Make filereader take single files

--- NeoCore/src/me/neoblade298/neocore/io/FileReader.java
+++ NeoCore/src/me/neoblade298/neocore/io/FileReader.java
@@ -8,20 +8,24 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import me.neoblade298.neocore.exceptions.NeoIOException;
 
 public class FileReader {
-	public static  void loadRecursive(File dir, FileLoader loader) throws NeoIOException {
-		for (File file : dir.listFiles()) {
-			if (file.isDirectory()) {
+	public static void loadRecursive(File load, FileLoader loader) throws NeoIOException {
+		if (!load.exists()) {
+			throw new NeoIOException("Failed to load file, doesn't exist: " + load.getParent() + "/" + load.getName());
+		}
+		
+		if (load.isDirectory()) {
+			for (File file : load.listFiles()) {
 				loadRecursive(file, loader);
 			}
-			else {
-				try {
-					YamlConfiguration cfg = YamlConfiguration.loadConfiguration(file);
-					loader.load(cfg);
-				}
-				catch (Exception e) {
-					Bukkit.getLogger().warning(e.getMessage());
-					throw new NeoIOException("Failed to parse yaml for file " + file.getParent() + "/" + file.getName());
-				}
+		}
+		else {
+			try {
+				YamlConfiguration cfg = YamlConfiguration.loadConfiguration(load);
+				loader.load(cfg);
+			}
+			catch (Exception e) {
+				Bukkit.getLogger().warning(e.getMessage());
+				throw new NeoIOException("Failed to parse yaml for file " + load.getParent() + "/" + load.getName());
 			}
 		}
 	}
