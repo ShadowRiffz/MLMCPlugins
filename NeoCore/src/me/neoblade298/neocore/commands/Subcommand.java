562077c2204d54b70686488d101d7e5174e700f4
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-06-04 17:46:05 -0700

    More cmd changes

--- NeoCore/src/me/neoblade298/neocore/commands/Subcommand.java
+++ NeoCore/src/me/neoblade298/neocore/commands/Subcommand.java
@@ -9,7 +9,7 @@ public interface Subcommand {
 	public String getKey();
 	public String getDescription();
 	public String getArgs();
-	public default ChatColor getColor() { return ChatColor.GRAY; }
+	public default ChatColor getColor() { return ChatColor.RED; }
 	public default boolean isHidden() { return false; }
 	
 	public void run(CommandSender s, String[] args);
