e9e3061422109fb6d175e38f2e616083176f9331
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-07-01 13:27:16 -0700

    Work on pvp kill handling

--- NeoPvp/src/me/neoblade298/neopvp/PvpAccount.java
+++ NeoPvp/src/me/neoblade298/neopvp/PvpAccount.java
@@ -3,11 +3,13 @@ package me.neoblade298.neopvp;
 import java.util.HashSet;
 import java.util.UUID;
 
+import org.bukkit.entity.Player;
+
 public class PvpAccount {
 	private UUID uuid;
 	private HashSet<UUID> uniqueKills;
 	private double pvpBalance;
-	private int elo;
+	private int elo, killstreak;
 	
 	public void addElo(int amount) {
 		elo += amount;
@@ -19,5 +21,37 @@ public class PvpAccount {
 	
 	public int getElo() {
 		return elo;
+	}
+	
+	public double getBalance() {
+		return pvpBalance;
+	}
+	
+	public void setBalance(double balance) {
+		pvpBalance = balance;
+	}
+	
+	public void addBalance(double balance) {
+		pvpBalance += balance;
+	}
+	
+	public int getNumUniqueKills() {
+		return uniqueKills.size();
+	}
+	
+	public void addUniqueKill(Player p) {
+		uniqueKills.add(p.getUniqueId());
+	}
+	
+	public void incrementKillstreak() {
+		killstreak++;
+	}
+	
+	public int getKillstreak() {
+		return killstreak;
+	}
+	
+	public void clearKillstreak() {
+		killstreak = 0;
 	}
 }
--- NeoPvp/src/me/neoblade298/neopvp/PvpManager.java
+++ NeoPvp/src/me/neoblade298/neopvp/PvpManager.java
@@ -5,9 +5,14 @@ import java.util.UUID;
 
 import org.bukkit.entity.Player;
 
+import me.neoblade298.neocore.NeoCore;
+
 public class PvpManager {
 	private static HashMap<UUID, PvpAccount> accounts;
 	private static final double MAX_ELO_GAIN = 24;
+	private static final double DEFAULT_KILL_GOLD = 500;
+	private static final int MAX_UNIQUE_KILLS = 100;
+	private static final double PCT_GOLD_KEPT = 0.9;
 	
 	public static PvpAccount getAccount(Player p) {
 		return accounts.get(p.getUniqueId());
@@ -24,11 +29,35 @@ public class PvpManager {
 		// Calculate elo
 		double killerElo = killerAcc.getElo();
 		double killedElo = killedAcc.getElo();
-		
 		double killerExpected = 1/(1+Math.pow(10, (killedElo - killerElo) / 400));
-		
 		int change = (int) (MAX_ELO_GAIN * (1 - killerExpected));
 		killerAcc.addElo(change);
 		killedAcc.takeElo(change);
+		
+		// Take money
+		double toTake = DEFAULT_KILL_GOLD * (1 + (Math.max(killerAcc.getNumUniqueKills(), MAX_UNIQUE_KILLS) * 0.05));
+		if (NeoCore.getEconomy().has(killed, toTake)) {
+			killerAcc.addUniqueKill(killed);
+		}
+		else {
+			toTake = NeoCore.getEconomy().getBalance(killed);
+		}
+		NeoCore.getEconomy().withdrawPlayer(killed, toTake);
+		killerAcc.addBalance(toTake * PCT_GOLD_KEPT);
+		
+		// Transfer pvp balance
+		killedAcc.setBalance(0);
+		killerAcc.addBalance(killedAcc.getBalance());
+		
+		// Killstreak
+		killerAcc.incrementKillstreak();
+		killedAcc.clearKillstreak();
+		
+		// TODO
+		handleWarKill();
+	}
+	
+	private static void handleWarKill() {
+		
 	}
 }
