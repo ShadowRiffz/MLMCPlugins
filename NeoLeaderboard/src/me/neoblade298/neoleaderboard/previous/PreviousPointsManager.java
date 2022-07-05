2289753073cfed1ff1deb63ae3ee7a9387f35f82
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-07-05 13:36:03 -0700

    Finish previouspointsmanager

--- NeoLeaderboard/src/me/neoblade298/neoleaderboard/previous/PreviousPointsManager.java
+++ NeoLeaderboard/src/me/neoblade298/neoleaderboard/previous/PreviousPointsManager.java
@@ -17,12 +17,12 @@ import me.neoblade298.neoleaderboard.points.PlayerPointType;
 import me.neoblade298.neoleaderboard.points.PointType;
 
 public class PreviousPointsManager {
-	private static TreeSet<PreviousEntry> topNations;
-	private static TreeSet<PreviousEntry> topTowns;
-	private static TreeSet<PreviousEntry> topPlayers;
-	private static HashMap<PointType, TreeSet<PreviousEntry>> topNationCategories;
-	private static HashMap<PlayerPointType, TreeSet<PreviousEntry>> topTownCategories;
-	private static HashMap<PlayerPointType, TreeSet<PreviousEntry>> topPlayerCategories;
+	private static TreeSet<PreviousEntry> topNations = new TreeSet<PreviousEntry>();
+	private static TreeSet<PreviousEntry> topTowns = new TreeSet<PreviousEntry>();
+	private static TreeSet<PreviousEntry> topPlayers = new TreeSet<PreviousEntry>();
+	private static HashMap<PointType, TreeSet<PreviousEntry>> topNationCategories = new HashMap<PointType, TreeSet<PreviousEntry>>();
+	private static HashMap<PlayerPointType, TreeSet<PreviousEntry>> topTownCategories = new HashMap<PlayerPointType, TreeSet<PreviousEntry>>();
+	private static HashMap<PlayerPointType, TreeSet<PreviousEntry>> topPlayerCategories = new HashMap<PlayerPointType, TreeSet<PreviousEntry>>();
 	
 	static {
 		new BukkitRunnable() {
@@ -36,8 +36,49 @@ public class PreviousPointsManager {
 						topNations.add(new PreviousEntry(uuid, rs.getString(2), PointType.getPointType(rs.getString(3)), rs.getDouble(4)));
 					}
 
+					rs = stmt.executeQuery("SELECT * FROM leaderboard_previous_towns WHERE category = 'TOTAL';");
+					while (rs.next()) {
+						UUID uuid = UUID.fromString(rs.getString(1));
+						topTowns.add(new PreviousEntry(uuid, rs.getString(2), PointType.getPointType(rs.getString(3)), rs.getDouble(4)));
+					}
+
+					rs = stmt.executeQuery("SELECT * FROM leaderboard_previous_players WHERE category = 'TOTAL';");
+					while (rs.next()) {
+						UUID uuid = UUID.fromString(rs.getString(1));
+						topPlayers.add(new PreviousEntry(uuid, PointType.getPointType(rs.getString(2)), rs.getDouble(3)));
+					}
+					
 					for (NationPointType type : NationPointType.values()) {
+						topNationCategories.put(type, new TreeSet<PreviousEntry>());
+						rs = stmt.executeQuery("SELECT * FROM leaderboard_previous_nations WHERE category = '" + type + "';");
+						while (rs.next()) {
+							UUID uuid = UUID.fromString(rs.getString(1));
+							topNationCategories.get(type).add(new PreviousEntry(uuid, rs.getString(2), PointType.getPointType(rs.getString(3)), rs.getDouble(4)));
+						}
+					}
+					
+					for (PlayerPointType type : PlayerPointType.values()) {
+						topNationCategories.put(type, new TreeSet<PreviousEntry>());
+						topTownCategories.put(type, new TreeSet<PreviousEntry>());
+						topPlayerCategories.put(type, new TreeSet<PreviousEntry>());
 						
+						rs = stmt.executeQuery("SELECT * FROM leaderboard_previous_nations WHERE category = '" + type + "';");
+						while (rs.next()) {
+							UUID uuid = UUID.fromString(rs.getString(1));
+							topNationCategories.get(type).add(new PreviousEntry(uuid, rs.getString(2), PointType.getPointType(rs.getString(3)), rs.getDouble(4)));
+						}
+						
+						rs = stmt.executeQuery("SELECT * FROM leaderboard_previous_towns WHERE category = '" + type + "';");
+						while (rs.next()) {
+							UUID uuid = UUID.fromString(rs.getString(1));
+							topNationCategories.get(type).add(new PreviousEntry(uuid, rs.getString(2), PointType.getPointType(rs.getString(3)), rs.getDouble(4)));
+						}
+
+						rs = stmt.executeQuery("SELECT * FROM leaderboard_previous_players WHERE category = '" + type + "';");
+						while (rs.next()) {
+							UUID uuid = UUID.fromString(rs.getString(1));
+							topNationCategories.get(type).add(new PreviousEntry(uuid, PointType.getPointType(rs.getString(2)), rs.getDouble(3)));
+						}
 					}
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
@@ -45,5 +86,29 @@ public class PreviousPointsManager {
 				}
 			}
 		}.runTaskAsynchronously(NeoLeaderboard.inst());
+	}
+
+	public static TreeSet<PreviousEntry> getTopNations() {
+		return topNations;
+	}
+
+	public static TreeSet<PreviousEntry> getTopTowns() {
+		return topTowns;
+	}
+
+	public static TreeSet<PreviousEntry> getTopPlayers() {
+		return topPlayers;
+	}
+
+	public static HashMap<PointType, TreeSet<PreviousEntry>> getTopNationCategories() {
+		return topNationCategories;
+	}
+
+	public static HashMap<PlayerPointType, TreeSet<PreviousEntry>> getTopTownCategories() {
+		return topTownCategories;
+	}
+
+	public static HashMap<PlayerPointType, TreeSet<PreviousEntry>> getTopPlayerCategories() {
+		return topPlayerCategories;
 	}
 }
