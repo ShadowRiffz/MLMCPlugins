4cad14af4f9f7aed378e7b6ed61279d16ff92d13
ShanaChans <CedarMid@gmail.com>
2022-07-08 16:05:16 -0600

    SellAll Plugin

---
+++ SellAll/src/me/ShanaChans/SellAll/Commands/SellAllSet.java
@@ -0,0 +1,62 @@
+package me.ShanaChans.SellAll.Commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import me.ShanaChans.SellAll.SellAllManager;
+import me.neoblade298.neocore.commands.Subcommand;
+import me.neoblade298.neocore.commands.SubcommandRunner;
+
+public class SellAllSet implements Subcommand
+{
+	@Override
+	public String getDescription() 
+	{
+		return "Set various things to sold cap";
+	}
+
+	@Override
+	public String getKey() 
+	{
+		return "set";
+	}
+
+	@Override
+	public String getPermission() 
+	{
+		return null;
+	}
+
+	@Override
+	public SubcommandRunner getRunner() 
+	{
+		return SubcommandRunner.PLAYER_ONLY;
+	}
+
+	@Override
+	public void run(CommandSender sender, String[] args) 
+	{
+		Player player = (Player) sender;
+		
+		if(args.length > 2)
+		{
+			if(Bukkit.getPlayer(args[0]) != null)
+			{
+				if(Material.valueOf(args[1].toUpperCase()) != null)
+				{
+					
+					try {
+						int value = Integer.parseInt(args[2]);
+						SellAllManager.getPlayers().get(Bukkit.getPlayer(args[0]).getUniqueId()).setSold(player, Material.valueOf(args[1].toUpperCase()), value);
+					} catch(NumberFormatException e){
+						
+					}
+				}
+			}
+		}
+		
+	}
+
+}
---
+++ SellAll/src/me/ShanaChans/SellAll/SellAllManager.java
@@ -0,0 +1,120 @@
+package me.ShanaChans.SellAll;
+import java.io.File;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.block.Chest;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import de.tr7zw.nbtapi.NBTItem;
+import me.ShanaChans.SellAll.Commands.SellAllCap;
+import me.ShanaChans.SellAll.Commands.SellAllCommand;
+import me.ShanaChans.SellAll.Commands.SellAllGive;
+import me.ShanaChans.SellAll.Commands.SellAllSet;
+import me.neoblade298.neocore.commands.CommandManager;
+
+
+public class SellAllManager extends JavaPlugin implements Listener
+{
+	private static HashMap<Material, Double> itemPrices = new HashMap<Material, Double>();
+	private static HashMap<UUID, SellAllPlayer> players = new HashMap<UUID, SellAllPlayer>();
+	private YamlConfiguration cfg;
+	
+	public void onEnable() 
+	{
+        Bukkit.getServer().getLogger().info("SellAll Enabled");
+        getServer().getPluginManager().registerEvents(this, this);
+        initCommands();
+        loadConfigs();  
+    }
+	
+    public void onDisable() 
+    {
+        org.bukkit.Bukkit.getServer().getLogger().info("SellAll Disabled");
+        super.onDisable();
+    }
+    
+    private void initCommands()
+    {
+    	CommandManager sellAll = new CommandManager("sellall", this);
+    	sellAll.register(new SellAllCommand());
+    	sellAll.register(new SellAllCap());
+    	sellAll.register(new SellAllSet());
+    	sellAll.register(new SellAllGive());
+    	sellAll.registerCommandList("help");
+    	this.getCommand("sellall").setExecutor(sellAll);
+    }
+    
+    public void loadConfigs() 
+    {
+		File cfg = new File(getDataFolder(), "config.yml");
+
+		// Save config if doesn't exist
+		if (!cfg.exists()) {
+			saveResource("config.yml", false);
+		}
+		
+		this.cfg = YamlConfiguration.loadConfiguration(cfg);
+		ConfigurationSection sec = this.cfg.getConfigurationSection("pricelist");
+		
+		for (String key : sec.getKeys(false))
+		{
+			if(Material.valueOf(key) == null)
+			{
+				Bukkit.getLogger().warning("Item failed to load: " + key);
+			}
+			else
+			{
+				itemPrices.put(Material.valueOf(key), sec.getDouble(key));
+			}
+		}
+		
+	}
+    
+    @EventHandler
+    public void rightClick(PlayerInteractEvent e) 
+    {
+    	Player player = e.getPlayer();
+        if(e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getHand() == EquipmentSlot.HAND && e.getClickedBlock().getType() == Material.CHEST &&  e.getItem() != null)
+        {
+        	NBTItem heldItem = new NBTItem(e.getItem());
+        	if(heldItem.hasKey("sellStick"))
+        	{
+        		e.setCancelled(true);
+        		Chest chest = (Chest) e.getClickedBlock().getState();
+            	SellAllManager.getPlayers().get(player.getUniqueId()).sellAll(chest.getBlockInventory(), player);
+        	}
+        }
+    }
+    
+    @EventHandler
+    public void join(PlayerJoinEvent e) 
+    {
+       if(!e.getPlayer().hasPlayedBefore() || !players.containsKey(e.getPlayer().getUniqueId()))
+       {
+    	   players.put(e.getPlayer().getUniqueId(), new SellAllPlayer());
+       }
+    }
+    
+    public static HashMap<UUID, SellAllPlayer> getPlayers() 
+    {
+		return players;
+	}
+    
+    public static HashMap<Material, Double> getItemPrices() 
+    {
+		return itemPrices;
+	}
+
+}
