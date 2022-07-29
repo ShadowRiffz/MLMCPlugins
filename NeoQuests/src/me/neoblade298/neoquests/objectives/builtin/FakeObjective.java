4af3c8c0c43c4b03073354b321f671953188d9a0
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-07-28 19:51:15 -0700

    Fix fake objectives double-completing quests

--- NeoQuests/src/me/neoblade298/neoquests/objectives/builtin/FakeObjective.java
+++ NeoQuests/src/me/neoblade298/neoquests/objectives/builtin/FakeObjective.java
@@ -1,22 +1,19 @@
 package me.neoblade298.neoquests.objectives.builtin;
 
-import org.bukkit.scheduler.BukkitRunnable;
-
 import me.neoblade298.neocore.io.LineConfig;
-import me.neoblade298.neoquests.NeoQuests;
 import me.neoblade298.neoquests.objectives.Objective;
-import me.neoblade298.neoquests.objectives.ObjectiveInstance;
 
 public class FakeObjective extends Objective {
 	private String line;
 	
 	public FakeObjective() {
 		super();
+		this.needed = 0;
 	}
 
 	public FakeObjective(LineConfig cfg) {
 		super(null, cfg);
-		line = cfg.getLine();
+		this.needed = cfg.getInt("needed", 0);
 	}
 
 	@Override
@@ -32,15 +29,5 @@ public class FakeObjective extends Objective {
 	@Override
 	public String getDisplay() {
 		return line;
-	}
-	
-	@Override
-	public void initialize(ObjectiveInstance oi) {
-		new BukkitRunnable() {
-			public void run() {
-				// This objective counts as completed by default
-				oi.setCount(1);
-			}
-		}.runTaskLater(NeoQuests.inst(), 20L);
 	}
 }
