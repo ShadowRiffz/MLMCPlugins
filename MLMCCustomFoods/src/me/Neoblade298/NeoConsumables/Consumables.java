2960558e1adc99848a6ec1c9ae13afb8db9762b0
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-08-01 17:50:40 -0700

    fix bug

--- MLMCCustomFoods/src/me/Neoblade298/NeoConsumables/Consumables.java
+++ MLMCCustomFoods/src/me/Neoblade298/NeoConsumables/Consumables.java
@@ -360,7 +360,7 @@ public class Consumables extends JavaPlugin implements Listener {
 
 		// If consumable is a food, continue only if setting is set
 		if (cons instanceof FoodConsumable) {
-			if (!((boolean) settings.getValue(p.getUniqueId(), "InventoryUse"))) {
+			if (!((boolean) settings.exists("InventoryUse", p.getUniqueId()))) {
 				return;
 			}
 		}
