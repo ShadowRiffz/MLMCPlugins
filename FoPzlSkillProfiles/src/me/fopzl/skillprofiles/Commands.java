28280d0e1de18de147ddfadb0209184004347772
FoPzl <officialsuperalex11@gmail.com>
2022-08-09 19:55:06 -0400

    Basic command setup

--- FoPzlSkillProfiles/src/me/fopzl/skillprofiles/Commands.java
+++ FoPzlSkillProfiles/src/me/fopzl/skillprofiles/Commands.java
@@ -1,9 +1,34 @@
 package me.fopzl.skillprofiles;
 
-import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 
 public class Commands implements CommandExecutor {
-	// TODO
+	SkillProfiles main;
+	
+	public Commands(SkillProfiles main) {
+		this.main = main;
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
+		if(args.length < 2) return false;
+		
+		// TODO: verify args[1] is an int
+		int slot = Integer.parseInt(args[1]);
+		
+		switch(args[0]) {
+			case "save":
+				main.Save((Player)sender, slot);
+				return true;
+			case "load":
+				main.Load((Player)sender, slot);
+				return true;
+			default:
+				return false;
+		}
+	}
 }
