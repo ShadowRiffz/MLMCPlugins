556cd958833a583ff0d910a3fa95b67531b5992d
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-08-11 16:13:26 -0700

    Work on towny bridge

--- TownyChatBridge/src/me/neoblade298/townychatbridge/other/InstanceListener.java
+++ TownyChatBridge/src/me/neoblade298/townychatbridge/other/InstanceListener.java
@@ -2,6 +2,7 @@ package me.neoblade298.townychatbridge.other;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.UUID;
@@ -17,36 +18,39 @@ import me.neoblade298.neocore.bungee.PluginMessageEvent;
 import me.neoblade298.townychatbridge.TownyChatBridge;
 
 public class InstanceListener implements Listener {
-	private HashMap<UUID, UUID> playerToTown = new HashMap<UUID, UUID>();
-	private HashMap<UUID, UUID> townToNation = new HashMap<UUID, UUID>();
-	private HashMap<UUID, HashSet<Player>> onlinePlayers = new HashMap<UUID, HashSet<Player>>();
-	
+	private HashSet<UUID> towns = new HashSet<UUID>();
+	private HashMap<UUID, UUID> townToNation = new HashMap<UUID, UUID>(); // Key town uuid, value nation uuid
+	private HashMap<UUID, HashSet<Player>> onlinePlayers = new HashMap<UUID, HashSet<Player>>(); // Key town uuid, value
+																									// player
+
 	public void initialize() {
 		new BukkitRunnable() {
 			public void run() {
 				try {
-					ResultSet rs = NeoCore.getStatement().executeQuery("SELECT b.uuid, c.uuid FROM MLMC.TOWNY_TOWNS as b,"
-							+ " MLMC.TOWNY_NATIONS as c WHERE b.nation = c.name;");
+					ResultSet rs = NeoCore.getStatement()
+							.executeQuery("SELECT b.uuid, c.uuid FROM MLMC.TOWNY_TOWNS as b,"
+									+ " MLMC.TOWNY_NATIONS as c WHERE b.nation = c.name;");
 					while (rs.next()) {
 						UUID tuuid = UUID.fromString(rs.getString(2));
 						UUID nuuid = UUID.fromString(rs.getString(1));
 						townToNation.put(tuuid, nuuid);
 					}
-				}
-				catch(SQLException e) {
+				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 			}
-		}
+		}.runTaskAsynchronously(TownyChatBridge.inst());
 	}
 
 	@EventHandler
 	public void onPluginMessage(PluginMessageEvent e) {
-		UUID town = UUID.fromString(e.getMessages().get(1));
-		if (onlinePlayers.containsKey(town)) {
-			for (Player p : onlinePlayers.get(town)) {
-				p.sendMessage(e.getMessages().get(0));
-			}
+		switch (e.getChannel()) {
+		case "townchatout":
+			handleTownChat(e);
+			break;
+		case "townyevent":
+			handleTownyEvent(e);
+			break;
 		}
 	}
 
@@ -56,29 +60,50 @@ public class InstanceListener implements Listener {
 		new BukkitRunnable() {
 			public void run() {
 				try {
-					ResultSet rs = NeoCore.getStatement().executeQuery("SELECT b.uuid, c.uuid FROM MLMC.TOWNY_RESIDENTS as a, MLMC.TOWNY_TOWNS as b, "
-							+ "MLMC.TOWNY_NATIONS as c WHERE a.town = b.name AND b.nation = c.name AND a.name = '" + p.getName() + "';");
-					
+					ResultSet rs = NeoCore.getStatement().executeQuery(
+							"SELECT b.uuid FROM MLMC.TOWNY_RESIDENTS as a, MLMC.TOWNY_TOWNS as b "
+									+ "WHERE a.town = b.name AND a.name = '"
+									+ p.getName() + "';");
+
 					if (!rs.next()) return;
 					String townuuid = rs.getString(1);
-					
-					// Add town
+
+					// Add to online players
 					if (townuuid.isEmpty()) return;
 					UUID tuuid = UUID.fromString(townuuid);
 					HashSet<Player> players = onlinePlayers.getOrDefault(tuuid, new HashSet<Player>());
 					players.add(p);
 					onlinePlayers.putIfAbsent(tuuid, players);
-					String nationuuid = rs.getString(2);
-					
-					// Add nation
-					if (!nationuuid.isEmpty()) return;
-					UUID nuuid = UUID.fromString(townuuid);
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
+
 			}
 		}.runTaskAsynchronously(TownyChatBridge.inst());
+	}
+
+	private void handleTownChat(PluginMessageEvent e) {
+		UUID town = UUID.fromString(e.getMessages().get(1));
+		if (onlinePlayers.containsKey(town)) {
+			for (Player p : onlinePlayers.get(town)) {
+				p.sendMessage(e.getMessages().get(0));
+			}
+		}
+	}
+
+	private void handleTownyEvent(PluginMessageEvent e) {
+		String type = e.getMessages().get(0);
+		ArrayList<String> msgs = e.getMessages();
+		switch (type) {
+		case "newtown": towns.add(UUID.fromString(e.getMessages().get(1)));
+		break;
+		case "newnation": townToNation.put(UUID.fromString(msgs.get(1)), UUID.fromString(msgs.get(2)));
+		break;
+		case "townjoinnation": townToNation.put(UUID.fromString(msgs.get(1)), UUID.fromString(msgs.get(2)));
+		break;
+		case "townleavenation": townToNation.remove(UUID.fromString(msgs.get(1)));
+		break;
+		}
 	}
 }
--- TownyChatBridge/src/me/neoblade298/townychatbridge/towny/TownyListener.java
+++ TownyChatBridge/src/me/neoblade298/townychatbridge/towny/TownyListener.java
@@ -1,13 +1,15 @@
 package me.neoblade298.townychatbridge.towny;
 
-import java.util.Arrays;
-
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.mineacademy.chatcontrol.api.ChatChannelEvent;
 
 import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.event.NationAddTownEvent;
+import com.palmergames.bukkit.towny.event.NationRemoveTownEvent;
+import com.palmergames.bukkit.towny.event.NewNationEvent;
+import com.palmergames.bukkit.towny.event.NewTownEvent;
 import com.palmergames.bukkit.towny.object.Town;
 
 import me.neoblade298.neocore.bungee.BungeeAPI;
@@ -16,8 +18,9 @@ public class TownyListener implements Listener {
 	TownyAPI api;
 	
 	@EventHandler
-	public void onTownyMessageTown(ChatChannelEvent e) {
+	public void onTownChat(ChatChannelEvent e) {
 		if (!(e.getSender() instanceof Player)) return;
+		if (!e.getChannel().getName().equalsIgnoreCase("town")) return;
 		Player p = (Player) e.getSender();
 		
 		if (api == null) api = TownyAPI.getInstance();
@@ -25,6 +28,40 @@ public class TownyListener implements Listener {
 		
 		// Only send message of players with a town
 		if (t == null) return;
-		BungeeAPI.sendPluginMessage(p, "townychatout", Arrays.asList(e.getMessage(), t.getUUID()));
+		BungeeAPI.sendPluginMessage(p, "townychatout", e.getMessage(), t.getUUID().toString());
+	}
+
+	@EventHandler
+	public void onCreateTown(NewTownEvent e) {
+		String[] list = new String[2];
+		list[0] = "newtown";
+		list[1] = e.getTown().getUUID().toString();
+		BungeeAPI.sendPluginMessage("townyevents", list);
+	}
+	@EventHandler
+	public void onCreateNation(NewNationEvent e) {
+		String[] list = new String[3];
+		list[0] = "newnation";
+		list[1] = e.getNation().getUUID().toString();
+		list[2] = e.getNation().getTowns().get(0).getUUID().toString();
+		BungeeAPI.sendPluginMessage("townyevents", list);
+	}
+
+	@EventHandler
+	public void onJoinNation(NationAddTownEvent e) {
+		String[] list = new String[3];
+		list[0] = "townjoinnation";
+		list[1] = e.getTown().getUUID().toString();
+		list[2] = e.getNation().getUUID().toString();
+		BungeeAPI.sendPluginMessage("townyevents", list);
+	}
+
+	@EventHandler
+	public void onLeaveNation(NationRemoveTownEvent e) {
+		String[] list = new String[3];
+		list[0] = "townleavenation";
+		list[1] = e.getTown().getUUID().toString();
+		list[2] = e.getNation().getUUID().toString();
+		BungeeAPI.sendPluginMessage("townyevents", list);
 	}
 }
