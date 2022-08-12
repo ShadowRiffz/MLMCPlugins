6896429a4b6dccf09b37e7cf551b221583e24351
FoPzl <officialsuperalex11@gmail.com>
2022-08-11 21:26:35 -0400

    Skill and bar saving+loading mostly functional

--- FoPzlSkillProfiles/src/me/fopzl/skillprofiles/SkillProfiles.java
+++ FoPzlSkillProfiles/src/me/fopzl/skillprofiles/SkillProfiles.java
@@ -1,17 +1,20 @@
 package me.fopzl.skillprofiles;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.sucy.skill.SkillAPI;
-import com.sucy.skill.api.player.PlayerAccounts;
 import com.sucy.skill.api.player.PlayerData;
+import com.sucy.skill.api.player.PlayerSkill;
+import com.sucy.skill.api.player.PlayerSkillBar;
+import com.sucy.skill.api.skills.Skill;
 
 public class SkillProfiles extends JavaPlugin {
 	HashMap<UUID, PlayerProfiles> playerProfiles = new HashMap<UUID, PlayerProfiles>(); 
@@ -43,7 +46,7 @@ public class SkillProfiles extends JavaPlugin {
 		profs.save(data, accId, profileName);
 	}
 	
-	// TODO: verify stuff (e.g. exists) before loading
+	// TODO: verify stuff (e.g. exists) before loading (IMPORTANT: check if on same class/whatever)
 	public void load(Player player, String profileName) {
 		PlayerData data = SkillAPI.getPlayerData(player);
 		int accId = SkillAPI.getPlayerAccountData(player).getActiveId();
@@ -87,11 +90,7 @@ class AccountProfiles {
 	HashMap<String, Profile> profiles = new HashMap<String, Profile>();
 	
 	void save(PlayerData data, String profileName) {
-		Profile profile = new Profile();
-		
-		profile.attributes = data.getInvestedAttributes();
-		
-		profiles.put(profileName, profile);
+		profiles.put(profileName, new Profile(data));
 	}
 	
 	void load(PlayerData data, String profileName) {
@@ -100,18 +99,72 @@ class AccountProfiles {
 			return;
 		}
 		
-		Profile profile = profiles.get(profileName);
-		
-		data.refundAttributes();
-		
-		for (Map.Entry<String, Integer> kvp : profile.attributes.entrySet()) {
-			for(int i = 0; i < kvp.getValue().intValue(); i++) {
-				data.upAttribute(kvp.getKey());
-			}
-		}
+		profiles.get(profileName).Load(data);
 	}
 }
 
 class Profile {
-	HashMap<String, Integer> attributes;
+	private HashMap<String, Integer> attributes;
+	private HashMap<String, Integer> skillLevels;
+	private HashMap<String, Material> skillBinds;
+	private HashMap<Integer, String> skillBar;
+	private boolean skillBarEnabled;
+	
+	public Profile(PlayerData data) {
+		/* attributes */
+		attributes = data.getInvestedAttributes();
+		
+		/* skills */
+		Collection<PlayerSkill> skills = data.getSkills();
+		skillLevels = new HashMap<String, Integer>();
+		skillBinds = new HashMap<String, Material>();
+		for(PlayerSkill ps : skills) {
+			String name = ps.getData().getName();
+			skillLevels.put(name, ps.getLevel());
+			skillBinds.put(name, ps.getBind());
+		}
+		
+		/* skill bar */
+		skillBar = new HashMap<Integer, String>();
+		skillBar.putAll(data.getSkillBar().getData());
+		skillBarEnabled = data.getSkillBar().isEnabled();
+	}
+	
+	public void Load(PlayerData data) {
+		/* attributes */
+		data.refundAttributes();
+		for (Map.Entry<String, Integer> kvp : attributes.entrySet()) {
+			for(int i = 0; i < kvp.getValue().intValue(); i++) {
+				data.upAttribute(kvp.getKey());
+			}
+		}
+		
+		/* skills */
+		Collection<PlayerSkill> skills = data.getSkills();
+		for(PlayerSkill ps : skills) {
+			int level = ps.getLevel();
+			for(int i = 0; i < level; i++) {
+				data.downgradeSkill(ps.getData());
+			}
+		}
+		for(PlayerSkill ps : skills) {
+			Skill s = ps.getData();
+			for(int i = 0; i < skillLevels.getOrDefault(s.getName(), 0); i++) {
+				data.upgradeSkill(s, true);
+			}
+			ps.setBind(skillBinds.get(s.getName()));
+		}
+		
+		/* skill bar */
+		PlayerSkillBar bar = data.getSkillBar();
+		
+		bar.clear(data.getPlayer());
+		bar.getData().clear();
+		bar.getData().putAll(skillBar);
+		bar.setup(data.getPlayer());
+		
+		if(bar.isEnabled() != skillBarEnabled) {
+			bar.toggleEnabled();
+		}
+	}
 }
