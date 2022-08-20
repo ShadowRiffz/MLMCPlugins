7fb7c6f04d5ae4d72dd69e33b87186f08a521fcb
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-08-20 12:03:01 -0700

    start working on nla addplayer

--- NeoLeaderboard/src/me/neoblade298/neoleaderboard/commands/CmdNLAAddPlayer.java
+++ NeoLeaderboard/src/me/neoblade298/neoleaderboard/commands/CmdNLAAddPlayer.java
@@ -1,32 +1,15 @@
 package me.neoblade298.neoleaderboard.commands;
 
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Formatter;
-import java.util.Iterator;
-import java.util.TreeSet;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
-
 import me.neoblade298.neocore.commands.CommandArgument;
 import me.neoblade298.neocore.commands.CommandArguments;
 import me.neoblade298.neocore.commands.Subcommand;
 import me.neoblade298.neocore.commands.SubcommandRunner;
 import me.neoblade298.neocore.util.Util;
-import me.neoblade298.neoleaderboard.NeoLeaderboard;
-import me.neoblade298.neoleaderboard.points.NationEntry;
 import me.neoblade298.neoleaderboard.points.PlayerPointType;
-import me.neoblade298.neoleaderboard.points.PointsManager;
-import me.neoblade298.neoleaderboard.points.TownEntry;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder.FormatRetention;
-import net.md_5.bungee.api.chat.hover.content.Text;
 
 public class CmdNLAAddPlayer implements Subcommand {
 	private static final CommandArguments args = new CommandArguments(Arrays.asList(new CommandArgument("player"),
