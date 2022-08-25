9129d2a41b91d8698adabce1f4f224a19099a926
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-08-24 23:49:43 -0700

    remove code i accidentally added

--- NeoConsumables/src/me/Neoblade298/NeoConsumables/Util.java
+++ NeoConsumables/src/me/Neoblade298/NeoConsumables/Util.java
@@ -17,23 +17,4 @@ public class Util {
 	public static void serverBroadcast(String msg) {
 		BungeeAPI.broadcast(msg);
 	}
-	
-	public static String connectArgs(String args[]) {
-		return connectArgs(args, 0, args.length);
-	}
-	
-	public static String connectArgs(String args[], int start) {
-		return connectArgs(args, start, args.length);
-	}
-	
-	public static String connectArgs(String args[], int start, int end) {
-		String connected = "";
-		for (int i = start; i < end; i++) {
-			connected += args[i];
-			if (i != end) {
-				connected += " ";
-			}
-		}
-		return connected;
-	}
 }
