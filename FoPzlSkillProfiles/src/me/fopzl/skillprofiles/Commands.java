dbaf28e4efdbc0b0e89a53c2771982e5f49c6e84
FoPzl <officialsuperalex11@gmail.com>
2022-08-28 15:14:53 -0400

    Added perm for console

--- FoPzlSkillProfiles/src/me/fopzl/skillprofiles/Commands.java
+++ FoPzlSkillProfiles/src/me/fopzl/skillprofiles/Commands.java
@@ -15,7 +15,15 @@ public class Commands implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String lbl, String[] args) {
-		if(args.length < 1 || !(sender instanceof Player)) return false;
+		if(args.length < 1) return false;
+		
+		if(!(sender instanceof Player)) {
+			if(args.length >= 1 && args[0] == "debug") {
+				return main.toggleDebug(sender);
+			} else {
+				return false;
+			}
+		}
 		
 		switch(args[0]) {
 			case "save":
