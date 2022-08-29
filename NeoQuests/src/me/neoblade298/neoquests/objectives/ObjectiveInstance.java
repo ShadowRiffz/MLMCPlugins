0faffa83fb782ef8c1d35e6a8ed39ec58ff77add
Alexander Zhou <32420795+Alexz1008@users.noreply.github.com>
2022-08-29 00:36:15 -0700

    Add corebar support

--- NeoQuests/src/me/neoblade298/neoquests/objectives/ObjectiveInstance.java
+++ NeoQuests/src/me/neoblade298/neoquests/objectives/ObjectiveInstance.java
@@ -2,6 +2,8 @@ package me.neoblade298.neoquests.objectives;
 
 import org.bukkit.entity.Player;
 
+import me.neoblade298.neocore.bar.BarAPI;
+import me.neoblade298.neocore.bar.CoreBar;
 import me.neoblade298.neoquests.listeners.ObjectiveListener;
 
 public class ObjectiveInstance {
@@ -30,6 +32,7 @@ public class ObjectiveInstance {
 
 	public boolean setCount(int count) {
 		this.count = Math.min(obj.getNeeded(), count);
+		updateBar();
 		if (isComplete()) {
 			set.checkCompletion();
 			return true;
@@ -40,6 +43,7 @@ public class ObjectiveInstance {
 	public boolean addCount(int count) {
 		this.count += count;
 		this.count = Math.min(obj.getNeeded(), this.count);
+		updateBar();
 		if (isComplete()) {
 			set.checkCompletion();
 			return true;
@@ -50,12 +54,31 @@ public class ObjectiveInstance {
 	public boolean incrementCount() {
 		if (this.count < obj.getNeeded()) {
 			this.count++;
+			updateBar();
 		}
 		if (isComplete()) {
 			set.checkCompletion();
 			return true;
 		}
 		return false;
+	}
+	
+	private void updateBar() {
+		CoreBar cb = BarAPI.getBar(p);
+		if (!cb.isEnabled()) return;
+		if (isComplete()) {
+			// Only increment progress bar if we're already following it
+			if (cb.getTopic().equals("" + obj.hashCode())) {
+				cb.setProgress(1);
+			}
+		}
+		else {
+			if (!cb.getTopic().equals("" + obj.hashCode()) && !obj.getDisplay().startsWith("Hide")) {
+				cb.setTitle("§7(§c/q§7)" + obj.getDisplay());
+			}
+			cb.setProgress((double) this.count / (double) obj.getNeeded());
+			cb.setTopic("" + obj.hashCode());
+		}
 	}
 
 	public boolean isComplete() {
